2b6167419340c5998919531bc3b96654de7af89a
Willy Tarreau <w@1wt.eu>
2017-01-06 17:41:29 +0100

    MEDIUM: server: split the address and the port into two different fields
    Keeping the address and the port in the same field causes a lot of problems,
    specifically on the DNS part where we're forced to cheat on the family to be
    able to keep the port. This causes some issues such as some families not being
    resolvable anymore.
    
    This patch first moves the service port to a new field "svc_port" so that the
    port field is never used anymore in the "addr" field (struct sockaddr_storage).
    All call places were adapted (there aren't that many).
    
    (cherry picked from commit 04276f3d6e146794edcb15fd599cade29b924129)

--- src/server.c
+++ src/server.c
@@ -1063,6 +1063,7 @@ int parse_server(const char *file, int linenum, char **args, struct proxy *curpr
 
  skip_name_resolution:
 			newsrv->addr = *sk;
+			newsrv->svc_port = get_host_port(sk);
 			newsrv->xprt  = newsrv->check.xprt = newsrv->agent.xprt = &raw_sock;
 
 			if (!protocol_by_family(newsrv->addr.ss_family)) {
@@ -2810,7 +2811,7 @@ const char *update_server_addr_port(struct server *s, const char *addr, const ch
 			chunk_appendf(msg, ", ");
 
 		/* collecting data currently setup */
-		current_port = get_host_port(&s->addr);
+		current_port = s->svc_port;
 
 		/* check if PORT change is required */
 		port_change_required = 0;
@@ -2851,7 +2852,7 @@ const char *update_server_addr_port(struct server *s, const char *addr, const ch
 		/* applying PORT changes if required and update response message */
 		if (port_change_required) {
 			/* apply new port */
-			set_host_port(&s->addr, new_port);
+			s->svc_port = new_port;
 
 			/* prepare message */
 			chunk_appendf(msg, "port changed from '");
