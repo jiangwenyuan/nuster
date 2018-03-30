0bdd3fdf91a2e55c17aad2004057f728d82055c1
Willy Tarreau <w@1wt.eu>
2018-03-30 17:41:19 +0200

    BUG/MINOR: h2: remove accidental debug code introduced with show_fd function
    Commit e3f36cd ("MINOR: h2: implement a basic "show_fd" function")
    accidently brought one surrounding debugging part that was in the same
    context. No backport needed.
    
    (cherry picked from commit 1a1dd6066fb3b3ea88969d277c6f5030b5b380ab)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/mux_h2.c
+++ src/mux_h2.c
@@ -3435,11 +3435,6 @@ static int h2_snd_buf(struct conn_stream *cs, struct buffer *buf, int flags)
 		/* stream flow control, quit the list */
 		LIST_DEL(&h2s->list);
 		LIST_INIT(&h2s->list);
-
-			if (!h2s->cs && LIST_ISEMPTY(&h2s->h2c->send_list) && LIST_ISEMPTY(&h2s->h2c->fctl_list)) {
-				fprintf(stderr, "%s:%d: removing %p\n", __FUNCTION__, __LINE__, h2s);
-			}
-
 	}
 	else if (LIST_ISEMPTY(&h2s->list)) {
 		if (h2s->flags & H2_SF_BLK_MFCTL)
