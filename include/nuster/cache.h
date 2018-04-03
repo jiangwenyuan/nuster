cd2830755f19049428c2798c05ab279d9b05ae2c
Jiang Wenyuan <koubunen@gmail.com>
2018-04-03 22:13:37 +0900

    Rename CACHE_DEFAULT_GROWTH_FACTOR

--- include/nuster/cache.h
+++ include/nuster/cache.h
@@ -36,7 +36,7 @@
 #define NST_CACHE_DEFAULT_TTL                 3600
 #define NST_CACHE_DEFAULT_DICT_SIZE           32
 #define NST_CACHE_DEFAULT_LOAD_FACTOR         0.75
-#define CACHE_DEFAULT_GROWTH_FACTOR       2
+#define NST_CACHE_DEFAULT_GROWTH_FACTOR       2
 #define CACHE_DEFAULT_KEY                "method.scheme.host.uri"
 #define CACHE_DEFAULT_CODE               "200"
 #define CACHE_DEFAULT_KEY_SIZE            128
