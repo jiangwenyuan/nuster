8f174c9c93773e9ad634dc5193db0e1f10a16e89
Jiang Wenyuan <koubunen@gmail.com>
2018-04-06 22:15:48 +0900

    Update cache_ctx

--- src/nuster/cache/dict.c
+++ src/nuster/cache/dict.c
@@ -198,7 +198,7 @@ void cache_dict_cleanup() {
 /*
  * Add a new nst_cache_entry to cache_dict
  */
-struct nst_cache_entry *cache_dict_set(const char *key, uint64_t hash, struct cache_ctx *ctx) {
+struct nst_cache_entry *cache_dict_set(const char *key, uint64_t hash, struct nst_cache_ctx *ctx) {
     struct nst_cache_dict  *dict  = NULL;
     struct nst_cache_data  *data  = NULL;
     struct nst_cache_entry *entry = NULL;
