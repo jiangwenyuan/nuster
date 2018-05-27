fc8dd507e08464687e6c13118278256995ece7ec
Jiang Wenyuan <koubunen@gmail.com>
2018-05-27 19:13:27 +0900

    Add rudimental nosql

--- src/nuster/nosql/engine.c
+++ src/nuster/nosql/engine.c
@@ -70,9 +70,9 @@ struct nst_nosql_data *nst_nosql_data_new() {
 
     nuster_shctx_lock(nuster.nosql);
     if(data) {
-        data->clients  = 0;
-        data->invalid  = 0;
-        data->element  = NULL;
+        data->clients = 0;
+        data->invalid = 0;
+        data->element = NULL;
 
         if(nuster.nosql->data_head == NULL) {
             nuster.nosql->data_head = data;
@@ -98,6 +98,7 @@ void nst_nosql_init() {
     nuster.applet.nosql_engine.fct = nst_nosql_engine_handler;
 
     if(global.nuster.nosql.status == NUSTER_STATUS_ON) {
+        global.nuster.nosql.pool.ctx   = create_pool("np.ctx", sizeof(struct nst_nosql_ctx), MEM_F_SHARED);
         global.nuster.nosql.memory = nuster_memory_create("nosql.shm", global.nuster.nosql.data_size, global.tune.bufsize, NST_NOSQL_DEFAULT_CHUNK_SIZE);
         if(!global.nuster.nosql.memory) {
             goto shm_err;
@@ -142,16 +143,18 @@ shm_err:
 int nst_nosql_check_applet(struct stream *s, struct channel *req, struct proxy *px) {
     if(global.nuster.nosql.status == NUSTER_STATUS_ON && px->nuster.mode == NUSTER_MODE_NOSQL) {
         struct stream_interface *si = &s->si[1];
-        struct http_txn *txn = s->txn;
-        struct http_msg *msg = &txn->req;
+        struct http_txn *txn        = s->txn;
+        struct http_msg *msg        = &txn->req;
         struct appctx *appctx       = NULL;
 
         s->target = &nuster.applet.nosql_engine.obj_type;
         if(unlikely(!stream_int_register_handler(si, objt_applet(s->target)))) {
+            txn->status = 500;
+            nuster_response(s, &nuster_http_msg_chunks[NUSTER_HTTP_500]);
             return 1;
         } else {
             appctx      = si_appctx(si);
-            appctx->st0 = 0;
+            appctx->st0 = NST_NOSQL_APPCTX_STATE_INIT;
             appctx->st1 = 0;
             appctx->st2 = 0;
 
@@ -185,3 +188,351 @@ int nst_nosql_check_applet(struct stream *s, struct channel *req, struct proxy *
     }
     return 0;
 }
+
+static char *_string_append(char *dst, int *dst_len, int *dst_size,
+        char *src, int src_len) {
+
+    int left     = *dst_size - *dst_len;
+    int need     = src_len + 1;
+    int old_size = *dst_size;
+
+    if(left < need) {
+        *dst_size += ((need - left) / NST_CACHE_DEFAULT_KEY_SIZE + 1)  * NST_CACHE_DEFAULT_KEY_SIZE;
+    }
+
+    if(old_size != *dst_size) {
+        char *new_dst = realloc(dst, *dst_size);
+        if(!new_dst) {
+            free(dst);
+            return NULL;
+        }
+        dst = new_dst;
+    }
+
+    memcpy(dst + *dst_len, src, src_len);
+    *dst_len += src_len;
+    dst[*dst_len] = '\0';
+    return dst;
+}
+static char *_nst_nosql_key_append(char *dst, int *dst_len, int *dst_size,
+        char *src, int src_len) {
+    char *key = _string_append(dst, dst_len, dst_size, src, src_len);
+    if(key) {
+        return _string_append(dst, dst_len, dst_size, ".", 1);
+    }
+    return NULL;
+}
+
+int nst_nosql_prebuild_key(struct nst_nosql_ctx *ctx, struct stream *s, struct http_msg *msg) {
+
+    struct http_txn *txn = s->txn;
+
+    char *url_end;
+    struct hdr_ctx hdr;
+
+    ctx->req.scheme = SCH_HTTP;
+#ifdef USE_OPENSSL
+    if(s->sess->listener->bind_conf->is_ssl) {
+        ctx->req.scheme = SCH_HTTPS;
+    }
+#endif
+
+    ctx->req.host.data = NULL;
+    ctx->req.host.len  = 0;
+    hdr.idx            = 0;
+    if(http_find_header2("Host", 4, msg->chn->buf->p, &txn->hdr_idx, &hdr)) {
+        ctx->req.host.data = nuster_memory_alloc(global.nuster.nosql.memory, hdr.vlen);
+        if(!ctx->req.host.data) {
+            return 0;
+        }
+        ctx->req.host.len  = hdr.vlen;
+        memcpy(ctx->req.host.data, hdr.line + hdr.val, hdr.vlen);
+    }
+
+    ctx->req.path.data = http_get_path(txn);
+    ctx->req.path.len  = 0;
+    ctx->req.uri.data  = ctx->req.path.data;
+    ctx->req.uri.len   = 0;
+    url_end            = NULL;
+    if(ctx->req.path.data) {
+        char *ptr = ctx->req.path.data;
+        url_end   = msg->chn->buf->p + msg->sl.rq.u + msg->sl.rq.u_l;
+        while(ptr < url_end && *ptr != '?') {
+            ptr++;
+        }
+        ctx->req.path.len = ptr - ctx->req.path.data;
+        ctx->req.uri.len  = url_end - ctx->req.uri.data;
+    }
+    /* extra 1 char as required by regex_exec_match2 */
+    ctx->req.path.data = nuster_memory_alloc(global.nuster.nosql.memory, ctx->req.path.len + 1);
+    if(!ctx->req.path.data) {
+        return 0;
+    }
+    memcpy(ctx->req.path.data, ctx->req.uri.data, ctx->req.path.len);
+
+    ctx->req.query.data = NULL;
+    ctx->req.query.len  = 0;
+    ctx->req.delimiter  = 0;
+    if(ctx->req.uri.data) {
+        ctx->req.query.data = memchr(ctx->req.uri.data, '?', url_end - ctx->req.uri.data);
+        if(ctx->req.query.data) {
+            ctx->req.query.data++;
+            ctx->req.query.len = url_end - ctx->req.query.data;
+            if(ctx->req.query.len) {
+                ctx->req.delimiter = 1;
+            }
+        }
+    }
+
+    ctx->req.cookie.data = NULL;
+    ctx->req.cookie.len  = 0;
+    hdr.idx              = 0;
+    if(http_find_header2("Cookie", 6, msg->chn->buf->p, &txn->hdr_idx, &hdr)) {
+        ctx->req.cookie.data = hdr.line + hdr.val;
+        ctx->req.cookie.len  = hdr.vlen;
+    }
+
+    return 1;
+}
+
+static int _nst_nosql_find_param_value_by_name(char *query_beg, char *query_end,
+        char *name, char **value, int *value_len) {
+
+    char equal   = '=';
+    char and     = '&';
+    char *ptr    = query_beg;
+    int name_len = strlen(name);
+
+    while(ptr + name_len + 1 < query_end) {
+        if(!memcmp(ptr, name, name_len) && *(ptr + name_len) == equal) {
+            if(ptr == query_beg || *(ptr - 1) == and) {
+                ptr    = ptr + name_len + 1;
+                *value = ptr;
+                while(ptr < query_end && *ptr != and) {
+                    (*value_len)++;
+                    ptr++;
+                }
+                return 1;
+            }
+        }
+        ptr++;
+    }
+    return 0;
+}
+
+char *nst_nosql_build_key(struct nst_nosql_ctx *ctx, struct nuster_rule_key **pck, struct stream *s,
+        struct http_msg *msg) {
+
+    struct http_txn *txn = s->txn;
+
+    struct hdr_ctx hdr;
+
+    struct nuster_rule_key *ck = NULL;
+    int key_len          = 0;
+    int key_size         = NST_NOSQL_DEFAULT_KEY_SIZE;
+    char *key            = malloc(key_size);
+    if(!key) {
+        return NULL;
+    }
+
+    nuster_debug("[CACHE] Calculate key: ");
+    while((ck = *pck++)) {
+        switch(ck->type) {
+            case NUSTER_RULE_KEY_METHOD:
+                nuster_debug("method.");
+                key = _nst_nosql_key_append(key, &key_len, &key_size, http_known_methods[txn->meth].name, strlen(http_known_methods[txn->meth].name));
+                break;
+            case NUSTER_RULE_KEY_SCHEME:
+                nuster_debug("scheme.");
+                key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.scheme == SCH_HTTPS ? "HTTPS" : "HTTP", ctx->req.scheme == SCH_HTTPS ? 5 : 4);
+                break;
+            case NUSTER_RULE_KEY_HOST:
+                nuster_debug("host.");
+                if(ctx->req.host.data) {
+                    key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.host.data, ctx->req.host.len);
+                }
+                break;
+            case NUSTER_RULE_KEY_URI:
+                nuster_debug("uri.");
+                if(ctx->req.uri.data) {
+                    key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.uri.data, ctx->req.uri.len);
+                }
+                break;
+            case NUSTER_RULE_KEY_PATH:
+                nuster_debug("path.");
+                if(ctx->req.path.data) {
+                    key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.path.data, ctx->req.path.len);
+                }
+                break;
+            case NUSTER_RULE_KEY_DELIMITER:
+                nuster_debug("delimiter.");
+                key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.delimiter ? "?": "", ctx->req.delimiter);
+                break;
+            case NUSTER_RULE_KEY_QUERY:
+                nuster_debug("query.");
+                if(ctx->req.query.data && ctx->req.query.len) {
+                    key = _nst_nosql_key_append(key, &key_len, &key_size, ctx->req.query.data, ctx->req.query.len);
+                }
+                break;
+            case NUSTER_RULE_KEY_PARAM:
+                nuster_debug("param_%s.", ck->data);
+                if(ctx->req.query.data && ctx->req.query.len) {
+                    char *v = NULL;
+                    int v_l = 0;
+                    if(_nst_nosql_find_param_value_by_name(ctx->req.query.data, ctx->req.query.data + ctx->req.query.len, ck->data, &v, &v_l)) {
+                        key = _nst_nosql_key_append(key, &key_len, &key_size, v, v_l);
+                    }
+
+                }
+                break;
+            case NUSTER_RULE_KEY_HEADER:
+                hdr.idx = 0;
+                nuster_debug("header_%s.", ck->data);
+                if(http_find_header2(ck->data, strlen(ck->data), msg->chn->buf->p, &txn->hdr_idx, &hdr)) {
+                    key = _nst_nosql_key_append(key, &key_len, &key_size, hdr.line + hdr.val, hdr.vlen);
+                }
+                break;
+            case NUSTER_RULE_KEY_COOKIE:
+                nuster_debug("header_%s.", ck->data);
+                if(ctx->req.cookie.data) {
+                    char *v = NULL;
+                    int v_l = 0;
+                    if(extract_cookie_value(ctx->req.cookie.data, ctx->req.cookie.data + ctx->req.cookie.len, ck->data, strlen(ck->data), 1, &v, &v_l)) {
+                        key = _nst_nosql_key_append(key, &key_len, &key_size, v, v_l);
+                    }
+                }
+                break;
+            case NUSTER_RULE_KEY_BODY:
+                nuster_debug("body.");
+                if(txn->meth == HTTP_METH_POST || txn->meth == HTTP_METH_PUT) {
+                    if((s->be->options & PR_O_WREQ_BODY) && msg->body_len > 0 ) {
+                        key = _nst_nosql_key_append(key, &key_len, &key_size, msg->chn->buf->p + msg->sov, msg->body_len);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        if(!key) return NULL;
+    }
+    nuster_debug("\n");
+    return key;
+}
+
+uint64_t nst_nosql_hash_key(const char *key) {
+    return XXH64(key, strlen(key), 0);
+}
+
+void nst_nosql_hit(struct stream *s, struct stream_interface *si, struct channel *req,
+        struct channel *res, struct nst_nosql_data *data) {
+}
+
+void nst_nosql_create(struct nst_nosql_ctx *ctx, char *key, uint64_t hash) {
+    struct nst_nosql_entry *entry = NULL;
+
+    ///* Check if nosql is full */
+    //if(nst_nosql_stats_full()) {
+    //    ctx->state = NST_nosql_CTX_STATE_FULL;
+    //    return;
+    //}
+
+    nuster_shctx_lock(&nuster.nosql->dict[0]);
+    entry = nst_nosql_dict_get(key, hash);
+    if(entry) {
+        if(entry->state == NST_NOSQL_ENTRY_STATE_CREATING) {
+            ctx->state = NST_NOSQL_CTX_STATE_WAIT;
+        } else {
+            entry->state = NST_NOSQL_ENTRY_STATE_CREATING;
+            if(entry->data) {
+                entry->data->invalid = 1;
+            }
+            entry->data = nst_nosql_data_new();
+        }
+    } else {
+        entry = nst_nosql_dict_set(key, hash, ctx);
+    }
+    nuster_shctx_unlock(&nuster.nosql->dict[0]);
+
+    if(!entry && !entry->data) {
+        ctx->state   = NST_NOSQL_CTX_STATE_INVALID;
+    } else {
+        ctx->state   = NST_NOSQL_CTX_STATE_CREATE;
+        ctx->entry   = entry;
+        ctx->data    = entry->data;
+        ctx->element = entry->data->element;
+    }
+}
+
+static struct nst_nosql_element *_nst_nosql_data_append(struct nst_nosql_element *tail,
+        struct http_msg *msg, long msg_len) {
+
+    struct nst_nosql_element *element = nuster_memory_alloc(global.nuster.nosql.memory, sizeof(*element));
+
+    if(element) {
+        char *data = msg->chn->buf->data;
+        char *p    = msg->chn->buf->p;
+        int size   = msg->chn->buf->size;
+
+        element->msg.data = nuster_memory_alloc(global.nuster.nosql.memory, msg_len);
+        if(!element->msg.data) return NULL;
+
+        if(p - data + msg_len > size) {
+            int right = data + size - p;
+            int left  = msg_len - right;
+            memcpy(element->msg.data, p, right);
+            memcpy(element->msg.data + right, data, left);
+        } else {
+            memcpy(element->msg.data, p, msg_len);
+        }
+        element->msg.len = msg_len;
+        element->next    = NULL;
+        if(tail == NULL) {
+            tail = element;
+        } else {
+            tail->next = element;
+        }
+        //nst_nosql_stats_update_used_mem(msg_len);
+    }
+    return element;
+}
+
+int nst_nosql_update(struct nst_nosql_ctx *ctx, struct http_msg *msg, long msg_len) {
+    struct nst_nosql_element *element = _nst_nosql_data_append(ctx->element, msg, msg_len);
+
+    if(element) {
+        if(!ctx->element) {
+            ctx->data->element = element;
+        }
+        ctx->element = element;
+        return 1;
+    } else {
+        return 0;
+    }
+}
+struct nst_nosql_data *nst_nosql_exists(const char *key, uint64_t hash) {
+    struct nst_nosql_entry *entry = NULL;
+    struct nst_nosql_data  *data  = NULL;
+
+    if(!key) return NULL;
+
+    nuster_shctx_lock(&nuster.nosql->dict[0]);
+    entry = nst_nosql_dict_get(key, hash);
+    if(entry && entry->state == NST_NOSQL_ENTRY_STATE_VALID) {
+        data = entry->data;
+        data->clients++;
+    }
+    nuster_shctx_unlock(&nuster.nosql->dict[0]);
+
+    return data;
+}
+
+void nst_nosql_finish(struct nst_nosql_ctx *ctx) {
+    ctx->state = NST_NOSQL_CTX_STATE_DONE;
+    ctx->entry->state = NST_NOSQL_ENTRY_STATE_VALID;
+    if(*ctx->rule->ttl == 0) {
+        ctx->entry->expire = 0;
+    } else {
+        ctx->entry->expire = get_current_timestamp() / 1000 + *ctx->rule->ttl;
+    }
+}
+
--- src/nuster/nosql/filter.c
+++ src/nuster/nosql/filter.c
@@ -19,7 +19,8 @@
 #include <proto/proto_http.h>
 #include <proto/stream_interface.h>
 
-#include <nuster/cache.h>
+#include <nuster/nuster.h>
+#include <nuster/nosql.h>
 
 static int _nst_nosql_filter_init(struct proxy *px, struct flt_conf *fconf) {
     return 0;
@@ -33,6 +34,23 @@ static int _nst_nosql_filter_check(struct proxy *px, struct flt_conf *fconf) {
 }
 
 static int _nst_nosql_filter_attach(struct stream *s, struct filter *filter) {
+    if(global.nuster.nosql.status != NUSTER_STATUS_ON) {
+        return 0;
+    }
+    if(!filter->ctx) {
+        struct nst_nosql_ctx *ctx = pool_alloc(global.nuster.nosql.pool.ctx);
+        if(ctx == NULL ) {
+            return 0;
+        }
+        ctx->state   = NST_NOSQL_CTX_STATE_INIT;
+        ctx->rule    = NULL;
+        ctx->stash   = NULL;
+        ctx->entry   = NULL;
+        ctx->data    = NULL;
+        ctx->element = NULL;
+        ctx->pid     = -1;
+        filter->ctx  = ctx;
+    }
     register_data_filter(s, &s->req, filter);
     //register_data_filter(s, &s->res, filter);
     return 1;
@@ -43,30 +61,103 @@ static void _nst_nosql_filter_detach(struct stream *s, struct filter *filter) {
 
 static int _nst_nosql_filter_http_headers(struct stream *s, struct filter *filter,
         struct http_msg *msg) {
-    if(!(msg->chn->flags & CF_ISRESP)) {
+    struct stream_interface *si = &s->si[1];
+    struct nst_nosql_ctx *ctx   = filter->ctx;
+    struct nuster_rule *rule    = NULL;
+    struct proxy *px            = s->be;
+    char *key                   = NULL;
+    uint64_t hash               = 0;
+    struct appctx *appctx       = si_appctx(si);
+    struct http_txn *txn        = s->txn;
+
+    if((msg->chn->flags & CF_ISRESP)) {
+        return 1;
     }
+
+    if(txn->meth != HTTP_METH_GET && txn->meth != HTTP_METH_POST) {
+        appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR_NOT_ALLOWED;
+        return 0;
+    }
+
+    if(ctx->state == NST_NOSQL_CTX_STATE_INIT) {
+        if(!nst_nosql_prebuild_key(ctx, s, msg)) {
+            appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR;
+            return 0;
+        }
+
+        list_for_each_entry(rule, &px->nuster.rules, list) {
+            nuster_debug("[NOSQL] Checking rule: %s\n", rule->name);
+            /* build key */
+            key = nst_nosql_build_key(ctx, rule->key, s, msg);
+            if(!key) {
+                appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR;
+                return 0;
+            }
+            nuster_debug("[NOSQL] Got key: %s\n", key);
+            hash = nst_nosql_hash_key(key);
+
+            if(s->txn->meth == HTTP_METH_GET) {
+                ctx->data = nst_nosql_exists(key, hash);
+                if(ctx->data) {
+                    nuster_debug("EXIST\n[NOSQL] Hit\n");
+                    /* OK, nosql exists */
+                    appctx->st0 = NST_NOSQL_APPCTX_STATE_HIT;
+                    appctx->ctx.nuster.nosql_engine.data = ctx->data;
+                    appctx->ctx.nuster.nosql_engine.element = ctx->data->element;
+                    return 1;
+                }
+
+                nuster_debug("NOT EXIST\n");
+                appctx->st0 = NST_NOSQL_APPCTX_STATE_NOT_FOUND;
+                return 0;
+            } else {
+                nuster_debug("[NOSQL] Checking if rule pass: ");
+                if(nst_cache_test_rule(rule, s, msg->chn->flags & CF_ISRESP)) {
+                    nuster_debug("PASS\n");
+                    nst_nosql_create(ctx, key, hash);
+                    break;
+                }
+                nuster_debug("FAIL\n");
+            }
+        }
+    }
+
+    if(ctx->state == NST_NOSQL_CTX_STATE_WAIT) {
+        appctx->st0 = NST_NOSQL_APPCTX_STATE_WAIT;
+        return 0;
+    }
+
+    if(ctx->state == NST_NOSQL_CTX_STATE_INVALID) {
+        appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR;
+        return 0;
+    }
+
     return 1;
 }
 
 static int _nst_nosql_filter_http_forward_data(struct stream *s, struct filter *filter,
         struct http_msg *msg, unsigned int len) {
-        char *data = msg->chn->buf->data;
-        char *p    = msg->chn->buf->p;
-        int size   = msg->chn->buf->size;
 
-        if(p - data + len > size) {
-            int right = data + size - p;
-            int left  = len - right;
-            fprintf(stderr, "\n==========\n%.*s\n-------------------\n", right, p);
-            fprintf(stderr, "\n==========\n%.*s\n-------------------\n", left, data);
-        } else {
-            fprintf(stderr, "\n==========\n%.*s\n-------------------\n", len, p);
+    struct stream_interface *si = &s->si[1];
+    struct appctx *appctx       = si_appctx(si);
+    struct nst_nosql_ctx *ctx   = filter->ctx;
+
+    if(ctx->state == NST_NOSQL_CTX_STATE_CREATE && !(msg->chn->flags & CF_ISRESP)) {
+        if(!nst_nosql_update(ctx, msg, len)) {
+            ctx->entry->state = NST_NOSQL_ENTRY_STATE_INVALID;
+            appctx->st0       = NST_NOSQL_APPCTX_STATE_ERROR;
         }
+    }
     return len;
 }
 
 static int _nst_nosql_filter_http_end(struct stream *s, struct filter *filter,
         struct http_msg *msg) {
+    struct nst_nosql_ctx *ctx = filter->ctx;
+
+    if(ctx->state == NST_NOSQL_CTX_STATE_CREATE && (msg->chn->flags & CF_ISRESP)) {
+        nst_nosql_finish(ctx);
+    }
     return 1;
 }
 
