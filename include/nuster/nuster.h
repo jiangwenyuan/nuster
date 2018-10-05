c179e1ff5c1f993e06d54f3c4eceb1880f0fd6f4
Jiang Wenyuan <koubunen@gmail.com>
2018-10-05 20:00:59 +0900

    Release v1.0.2.17

--- include/nuster/nuster.h
+++ include/nuster/nuster.h
@@ -22,7 +22,7 @@
 #ifndef _NUSTER_H
 #define _NUSTER_H
 
-#define NUSTER_VERSION    "1.0.1.17"
+#define NUSTER_VERSION    "1.0.2.17"
 #define NUSTER_COPYRIGHT  "2017-2018, Jiang Wenyuan, <koubunen AT gmail DOT com >"
 
 #include <nuster/cache.h>
