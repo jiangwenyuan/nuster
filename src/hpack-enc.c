e5996c6b51364f18f8fbf80b16636c3f00c015f8
Willy Tarreau <w@1wt.eu>
2018-12-11 06:46:03 +0100

    BUG/MINOR: hpack: fix off-by-one in header name encoding length calculation
    In hpack_encode_header() there is a length check to verify that a literal
    header name fits in the buffer, but there it an off-by-one in this length
    check, which forgets the byte required to mark the encoding type (literal
    without indexing). It should be harmless though as it cannot be triggered
    since response headers passing through haproxy are limited by the reserve,
    which is not the case of the output buffer.
    
    This fix should be backported to 1.8.
    
    (cherry picked from commit 7571015939eb0ab560df0fefc434e273ec9d93ff)
    [wt: adjust context]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/hpack-enc.c
+++ src/hpack-enc.c
@@ -106,7 +106,7 @@ int hpack_encode_header(struct chunk *out, const struct ist n, const struct ist 
 		out->str[len++] = 0x58; // literal with indexing -- name="cache-control" (idx 24)
 	else if (isteq(n, ist("content-length")))
 		out->str[len++] = 0x5c; // literal with indexing -- name="content-length" (idx 28)
-	else if (len_to_bytes(n.len) && len + len_to_bytes(n.len) + n.len <= size) {
+	else if (len_to_bytes(n.len) && len + 1 + len_to_bytes(n.len) + n.len <= size) {
 		out->str[len++] = 0x00;      /* literal without indexing -- new name */
 
 		len = hpack_encode_len(out->str, len, n.len);
