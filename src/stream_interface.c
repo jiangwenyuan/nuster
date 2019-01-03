374ecb8343f267314e77710ec46ba30e5f3817f7
Christopher Faulet <cfaulet@haproxy.com>
2019-01-03 16:24:54 +0100

    BUG/MAJOR: stream-int: Update the stream expiration date in stream_int_notify()
    Since a long time, the expiration date of a stream is only updated in
    process_stream(). It is calculated, among others, using the channels expiration
    dates for reads and writes (.rex and .wex values). But these values are updated
    by the stream-interface. So when this happens at the connection layer, the
    update is only done if the stream's task is woken up. Otherwise, the stream
    expiration date is not immediatly updated. This leads to unexpected
    behaviours. Time to time, users reported that the wrong timeout was hitted or
    the wrong termination state was reported. This is partly because of this
    bug.
    
    Recently, we observed some blocked sessions for a while when big objects are
    served from the cache applet. It seems only concern the clients not reading the
    response. Because delivered objects are big, not all data can be sent. And
    because delivered objects are big, data are fast forwarded (from the input to
    the output with no stream wakeup). So in such situation, the stream expiration
    date is never updated and no timeout is hitted. The session remains blocked
    while the client remains connected.
    
    This bug exists at least since HAProxy 1.5. But recent changes on the connection
    layer make it more visible. It must be backported from 1.9 to 1.6. And with more
    pain it should be backported to 1.5.
    
    (cherry picked from commit d7607de06574f00e999434051e082615908939a6)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit dcb673d23ec09af5b4cad93b34b30bc31a3495f4)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit ca3a8768ddf3766db6b4b9e261c891c7d12ecb09)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/stream_interface.c
+++ src/stream_interface.c
@@ -451,6 +451,7 @@ void stream_int_notify(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 	struct channel *oc = si_oc(si);
+	struct task *task = si_task(si);
 
 	/* process consumer side */
 	if (channel_is_empty(oc)) {
@@ -547,7 +548,14 @@ void stream_int_notify(struct stream_interface *si)
 	      ((oc->flags & CF_WAKE_WRITE) &&
 	       (si_opposite(si)->state != SI_ST_EST ||
 	        (channel_is_empty(oc) && !oc->to_forward)))))) {
-		task_wakeup(si_task(si), TASK_WOKEN_IO);
+		task_wakeup(task, TASK_WOKEN_IO);
+	}
+	else {
+		/* Update expiration date for the task and requeue it */
+		task->expire = tick_first((tick_is_expired(task->expire, now_ms) ? 0 : task->expire),
+					  tick_first(tick_first(ic->rex, ic->wex),
+						     tick_first(oc->rex, oc->wex)));
+		task_queue(task);
 	}
 	if (ic->flags & CF_READ_ACTIVITY)
 		ic->flags &= ~CF_READ_DONTWAIT;
