2cde3c796833ab2ab3f3aebe10625d660a1d6f55
Kevin Zhu <ipandtcp@gmail.com>
2019-01-30 16:01:21 +0800

    BUG/MINOR: deinit: tcp_rep.inspect_rules not deinit, add to deinit
    It seems like this can be backported as far as 1.5.
    
    (cherry picked from commit 13ebef7ecb94168039241bce66c2bdc0a3789c16)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit c6358e374dd040c6b4bc5e6d6933f2b9a1b213d2)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit b7b08a3d30fc8037a72619da1bb9c902678797a1)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/haproxy.c
+++ src/haproxy.c
@@ -1529,6 +1529,7 @@ void deinit(void)
 		}
 
 		deinit_tcp_rules(&p->tcp_req.inspect_rules);
+		deinit_tcp_rules(&p->tcp_rep.inspect_rules);
 		deinit_tcp_rules(&p->tcp_req.l4_rules);
 
 		deinit_stick_rules(&p->storersp_rules);
