haproxy/haproxy

BUG/MINOR: spoe: corrected fragmentation string size
This patch must be backported to 1.9 and 1.8.

src/flt_spoe.c
@@ -450,7 +450,7 @@ spoe_prepare_hahello_frame(struct appctx *appctx, char *frame, size_t size)
 	if (agent != NULL && (agent->flags & SPOE_FL_RCV_FRAGMENTATION)) {
 		if (chk->data) chk->area[chk->data++] = ',';
 		memcpy(chk->area+chk->data, "fragmentation", 13);
-		chk->data += 5;
+		chk->data += 13;
 	}
 	if (spoe_encode_buffer(chk->area, chk->data, &p, end) == -1)
 		goto too_big;

jiangwenyuan/nuster

(cherry picked from commit 6b3690bc6ae44c60677d55c6a82b459f76b91e30)
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit eac5bea548dd07d82af12ef91f47a2e908e52bd7)
[wt: adjusted to new buffer API]

src/flt_spoe.c
@@ -444,7 +444,7 @@ spoe_prepare_hahello_frame(struct appctx *appctx, char *frame, size_t size)
 	if (agent != NULL && (agent->flags & SPOE_FL_RCV_FRAGMENTATION)) {
 		if (chk->len) chk->str[chk->len++] = ',';
 		memcpy(chk->str+chk->len, "fragmentation", 13);
-		chk->len += 5;
+		chk->len += 13;
 	}
 	if (spoe_encode_buffer(chk->str, chk->len, &p, end) == -1)
 		goto too_big;