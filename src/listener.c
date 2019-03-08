113cda2cd3a730647947f8bd6f1effc17efbe5c1
Olivier Houchard <ohouchard@haproxy.com>
2019-03-08 15:35:42 +0100

    BUG/MEDIUM: listeners: Don't call fd_stop_recv() if fd_updt is NULL.
    In do_unbind_listener, don't bother calling fd_stop_recv() if fd_updt is
    NULL. It means it has already been free'd, and it would crash.
    
    (cherry picked from commit a51885621d5bf8fa63a82e8beb8438841098251f)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/listener.c
+++ src/listener.c
@@ -320,7 +320,7 @@ void dequeue_all_listeners(struct list *list)
  */
 void do_unbind_listener(struct listener *listener, int do_close)
 {
-	if (listener->state == LI_READY)
+	if (listener->state == LI_READY && fd_updt)
 		fd_stop_recv(listener->fd);
 
 	if (listener->state == LI_LIMITED) {
