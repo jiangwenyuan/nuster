7b1a22e72bc9be65f6937d31a4f0decc279d1456
Christopher Faulet <cfaulet@haproxy.com>
2019-03-28 11:41:39 +0100

    BUG/MEDIUM: htx: Fix the process of HTTP CONNECT with h2 connections
    In HTX, the HTTP tunneling does not work if h1 and h2 are mixed (an h1 client
    sending requests to an h2 server or this opposite) because the h1 multiplexer
    always adds an EOM before switching it to tunnel mode. The h2 multiplexer
    interprets it as an end of stream, closing the stream as for any other
    transaction.
    
    To make it works again, we need to swith to the tunnel mode without emitting any
    EOM blocks. Because of that, HTX analyzers have been updated to switch the
    transaction to tunnel mode before end of the message (because there is no end of
    message...).
    
    To be consistent, the protocol switching is also handled the same way even
    though the 101 responses are not supported in h2.
    
    This patch must be backported to 1.9.
    
    (cherry picked from commit c62c2b9d9224daf453bdc73cad75c95cdff36e59)
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>

--- src/proto_htx.c
+++ src/proto_htx.c
@@ -1243,6 +1243,11 @@ int htx_request_forward_body(struct stream *s, struct channel *req, int an_bit)
 			htx->extra -= channel_htx_forward(req, htx, htx->extra);
 	}
 
+	if (txn->meth == HTTP_METH_CONNECT) {
+		msg->msg_state = HTTP_MSG_TUNNEL;
+		goto done;
+	}
+
 	/* Check if the end-of-message is reached and if so, switch the message
 	 * in HTTP_MSG_DONE state.
 	 */
@@ -2162,12 +2167,10 @@ int htx_response_forward_body(struct stream *s, struct channel *res, int an_bit)
 			htx->extra -= channel_htx_forward(res, htx, htx->extra);
 	}
 
-	if (!(msg->flags & HTTP_MSGF_XFER_LEN)) {
-		/* The server still sending data that should be filtered */
-		if (res->flags & CF_SHUTR || !HAS_RSP_DATA_FILTERS(s)) {
-			msg->msg_state = HTTP_MSG_TUNNEL;
-			goto done;
-		}
+	if ((txn->meth == HTTP_METH_CONNECT && txn->status == 200) || txn->status == 101 ||
+	    (!(msg->flags & HTTP_MSGF_XFER_LEN) && (res->flags & CF_SHUTR || !HAS_RSP_DATA_FILTERS(s)))) {
+		msg->msg_state = HTTP_MSG_TUNNEL;
+		goto done;
 	}
 
 	/* Check if the end-of-message is reached and if so, switch the message
