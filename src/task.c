05b893fc52c76187d3cae17d59bbff457b8788ca
Willy Tarreau <w@1wt.eu>
2019-04-15 18:52:40 +0200

    BUILD: task/thread: fix single-threaded build of task.c
    As expected, commit cde7902ac ("MEDIUM: tasks: improve fairness between
    the local and global queues") broke the build with threads disabled,
    and I forgot to rerun this test before committing. No backport is
    needed.
    
    (cherry picked from commit 3466e3cdcbdd637c54cc8d3090007f5c0a23ea03)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/task.c
+++ src/task.c
@@ -339,6 +339,7 @@ void process_runnable_tasks()
 
 	while (task_per_thread[tid].task_list_size < max_processed) {
 		if ((global_tasks_mask & tid_bit) && !grq) {
+#ifdef USE_THREAD
 			HA_SPIN_LOCK(TASK_RQ_LOCK, &rq_lock);
 			grq = eb32sc_lookup_ge(&rqueue, rqueue_ticks - TIMER_LOOK_BACK, tid_bit);
 			if (unlikely(!grq)) {
@@ -348,6 +349,7 @@ void process_runnable_tasks()
 					HA_ATOMIC_AND(&global_tasks_mask, ~tid_bit);
 				}
 			}
+#endif
 		}
 
 		/* If a global task is available for this thread, it's in grq
@@ -368,6 +370,7 @@ void process_runnable_tasks()
 			lrq = eb32sc_next(lrq, tid_bit);
 			__task_unlink_rq(t);
 		}
+#ifdef USE_THREAD
 		else {
 			t = eb32sc_entry(grq, struct task, rq);
 			grq = eb32sc_next(grq, tid_bit);
@@ -380,6 +383,7 @@ void process_runnable_tasks()
 				}
 			}
 		}
+#endif
 
 		/* Make sure nobody re-adds the task in the runqueue */
 		HA_ATOMIC_OR(&t->state, TASK_RUNNING);
