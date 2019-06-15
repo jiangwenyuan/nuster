e1023f37b1dd7b0303970e8a99767cd2dc9a41f6
Jiang Wenyuan <koubunen@gmail.com>
2019-06-15 22:51:35 +0900

    Add nuster_persist_exists

--- src/nuster/persist.c
+++ src/nuster/persist.c
@@ -10,6 +10,8 @@
  *
  */
 
+#include <dirent.h>
+
 #include <types/global.h>
 
 #include <nuster/memory.h>
@@ -38,5 +40,76 @@ char *nuster_persist_create(struct nuster_memory *m, uint64_t hash) {
     }
 
     return p;
+}
+
+static int
+_persist_valid(struct persist *disk, struct buffer *key, uint64_t hash) {
+
+    disk->fd = nuster_persist_open_read(disk->file);
+
+    if(disk->fd == -1) {
+        return NUSTER_ERR;
+    }
+
+    if(read(disk->fd, disk->meta, 48) == -1) {
+        close(disk->fd);
+        return NUSTER_ERR;
+    }
+
+    if(memcmp(disk->meta, "NUSTER", 6) !=0) {
+        return NUSTER_ERR;
+    }
+
+    if(*(uint64_t *)(disk->meta + NUSTER_PERSIST_META_INDEX_HASH) == hash
+            && *(uint64_t *)(disk->meta + NUSTER_PERSIST_META_INDEX_KEY_LEN)
+            == key->data && memcmp(disk->meta + NUSTER_PERSIST_META_INDEX_KEY,
+                key->area, key->data) == 0) {
+
+        return NUSTER_OK;
+    }
+
+    return NUSTER_ERR;
+}
+
+
+int
+nuster_persist_exists(struct persist *disk, struct buffer *key, uint64_t hash) {
+
+    if(disk->file) {
+        return _persist_valid(disk, key, hash);
+    } else {
+
+        struct dirent *de;
+        DIR *dir;
+
+        disk->file = nuster_memory_alloc(global.nuster.cache.memory,
+                NUSTER_FILE_LEN + 1);
+
+        sprintf(disk->file, "%s/%"PRIx64"/%02"PRIx64"/%016"PRIx64,
+                global.nuster.cache.directory, hash >> 60, hash >> 56, hash);
+
+        dir = opendir(disk->file);
+
+        if(!dir) {
+            return NUSTER_ERR;
+        }
+
+        while((de = readdir(dir)) != NULL) {
+
+            if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+                memcpy(disk->file + NUSTER_PATH_LEN, "/", 1);
+                memcpy(disk->file + NUSTER_PATH_LEN + 1, de->d_name,
+                        strlen(de->d_name));
+
+                if(_persist_valid(disk, key, hash) == NUSTER_OK) {
+                    closedir(dir);
+                    return NUSTER_OK;
+                }
+            }
+        }
+
+        closedir(dir);
+        return NUSTER_ERR;
+    }
 }
 
