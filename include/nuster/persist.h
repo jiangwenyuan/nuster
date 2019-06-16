be142569d9d58619dc81fe76007a287480a81ae8
Jiang Wenyuan <koubunen@gmail.com>
2019-06-16 13:05:30 +0900

    Shorten persist meta

--- include/nuster/persist.h
+++ include/nuster/persist.h
@@ -45,12 +45,12 @@
    48 + key_len cache_len       cache
  */
 
-#define NUSTER_PERSIST_META_INDEX_HASH          8 * 1
-#define NUSTER_PERSIST_META_INDEX_EXPIRE        8 * 2
-#define NUSTER_PERSIST_META_INDEX_CACHE_LEN     8 * 3
-#define NUSTER_PERSIST_META_INDEX_HEADER_LEN    8 * 4
-#define NUSTER_PERSIST_META_INDEX_KEY_LEN       8 * 5
-#define NUSTER_PERSIST_META_INDEX_KEY           8 * 6
+#define NUSTER_PERSIST_META_POS_HASH          8 * 1
+#define NUSTER_PERSIST_META_POS_EXPIRE        8 * 2
+#define NUSTER_PERSIST_META_POS_CACHE_LEN     8 * 3
+#define NUSTER_PERSIST_META_POS_HEADER_LEN    8 * 4
+#define NUSTER_PERSIST_META_POS_KEY_LEN       8 * 5
+#define NUSTER_PERSIST_META_POS_KEY           8 * 6
 
 struct persist {
     char         *file;             /* cache file */
@@ -82,43 +82,43 @@ static inline int nuster_persist_open(const char *pathname) {
 }
 
 static inline void nuster_persist_meta_set_hash(char *p, uint64_t v) {
-    *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_HASH) = v;
+    *(uint64_t *)(p + NUSTER_PERSIST_META_POS_HASH) = v;
 }
 
 static inline uint64_t nuster_persist_meta_get_hash(char *p) {
-    return *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_HASH);
+    return *(uint64_t *)(p + NUSTER_PERSIST_META_POS_HASH);
 }
 
 static inline void nuster_persist_meta_set_expire(char *p, uint64_t v) {
-    *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_EXPIRE) = v;
+    *(uint64_t *)(p + NUSTER_PERSIST_META_POS_EXPIRE) = v;
 }
 
 static inline uint64_t nuster_persist_meta_get_expire(char *p) {
-    return *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_EXPIRE);
+    return *(uint64_t *)(p + NUSTER_PERSIST_META_POS_EXPIRE);
 }
 
 static inline void nuster_persist_meta_set_cache_len(char *p, uint64_t v) {
-    *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_CACHE_LEN) = v;
+    *(uint64_t *)(p + NUSTER_PERSIST_META_POS_CACHE_LEN) = v;
 }
 
 static inline uint64_t nuster_persist_meta_get_cache_len(char *p) {
-    return *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_CACHE_LEN);
+    return *(uint64_t *)(p + NUSTER_PERSIST_META_POS_CACHE_LEN);
 }
 
 static inline void nuster_persist_meta_set_header_len(char *p, uint64_t v) {
-    *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_HEADER_LEN) = v;
+    *(uint64_t *)(p + NUSTER_PERSIST_META_POS_HEADER_LEN) = v;
 }
 
 static inline uint64_t nuster_persist_meta_get_header_len(char *p) {
-    return *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_HEADER_LEN);
+    return *(uint64_t *)(p + NUSTER_PERSIST_META_POS_HEADER_LEN);
 }
 
 static inline void nuster_persist_meta_set_key_len(char *p, uint64_t v) {
-    *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_KEY_LEN) = v;
+    *(uint64_t *)(p + NUSTER_PERSIST_META_POS_KEY_LEN) = v;
 }
 
 static inline uint64_t nuster_persist_meta_get_key_len(char *p) {
-    return *(uint64_t *)(p + NUSTER_PERSIST_META_INDEX_KEY_LEN);
+    return *(uint64_t *)(p + NUSTER_PERSIST_META_POS_KEY_LEN);
 }
 
 static inline void
