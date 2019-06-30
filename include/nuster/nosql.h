960d4cc225e940e7670960c434e0d251ff4d5286
Jiang Wenyuan <koubunen@gmail.com>
2019-06-30 23:11:38 +0900

    Simplify nst_nosql_dict_set

--- include/nuster/nosql.h
+++ include/nuster/nosql.h
@@ -217,8 +217,7 @@ int nst_nosql_get_headers(struct nst_nosql_ctx *ctx, struct stream *s,
 /* dict */
 int nst_nosql_dict_init();
 struct nst_nosql_entry *nst_nosql_dict_get(struct buffer *key, uint64_t hash);
-struct nst_nosql_entry *nst_nosql_dict_set(struct buffer *key, uint64_t hash,
-        struct nst_nosql_ctx *ctx);
+struct nst_nosql_entry *nst_nosql_dict_set(struct nst_nosql_ctx *ctx);
 
 void nst_nosql_dict_rehash();
 void nst_nosql_dict_cleanup();
