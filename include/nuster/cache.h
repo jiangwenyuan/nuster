5b68b6effab74c20950f061112225da61015c044
Jiang Wenyuan <koubunen@gmail.com>
2019-06-30 12:21:39 +0900

    Use common disk_loader

--- include/nuster/cache.h
+++ include/nuster/cache.h
@@ -44,7 +44,6 @@
 #define NST_CACHE_DEFAULT_CHUNK_SIZE          32
 #define NST_CACHE_DEFAULT_PURGE_METHOD       "PURGE"
 #define NST_CACHE_DEFAULT_PURGE_METHOD_SIZE   16
-#define NST_CACHE_DEFAULT_DISK_LOADER         1024
 #define NST_CACHE_DEFAULT_DISK_SAVER          1024
 
 struct nst_cache_element {
