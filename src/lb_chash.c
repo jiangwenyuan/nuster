haproxy/haproxy

BUG/MEDIUM: lb-chash: Fix the realloc() when the number of nodes is increased
When the number of nodes is increased because the server weight is changed, the
nodes array must be realloc. But its new size is not correctly set. Only the
total number of nodes is used to set the new size. But it must also depends on
the size of a node. It must be the total nomber of nodes times the size of a
node.

This issue was reported on Github (#189).

This patch must be backported to all versions since the 1.6.

src/lb_chash.c
@@ -84,7 +84,7 @@ static inline void chash_queue_dequeue_srv(struct server *s)
 	 * increased the weight beyond the original weight
 	 */
 	if (s->lb_nodes_tot < s->next_eweight) {
-		struct tree_occ *new_nodes = realloc(s->lb_nodes, s->next_eweight);
+		struct tree_occ *new_nodes = realloc(s->lb_nodes, s->next_eweight * sizeof(*new_nodes));
 
 		if (new_nodes) {
 			unsigned int j;

jiangwenyuan/nuster

(cherry picked from commit 366ad86af72c455cc958943913cb2de20eefee71)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit 974c6916ba2f7efc83193bb8c04e95294ca21112)
(cherry picked from commit 4e633502827c2f8b607d71ae01dc5a6ec70ba785)
(cherry picked from commit 5c0e731a031057099c2f42ef1807f2454badc983)
[wt: s/next_eweight/eweight]
Signed-off-by: Willy Tarreau <w@1wt.eu>

src/lb_chash.c
@@ -82,7 +82,7 @@ static inline void chash_queue_dequeue_srv(struct server *s)
 	 * increased the weight beyond the original weight
 	 */
 	if (s->lb_nodes_tot < s->eweight) {
-		struct tree_occ *new_nodes = realloc(s->lb_nodes, s->eweight);
+		struct tree_occ *new_nodes = realloc(s->lb_nodes, s->eweight * sizeof(*new_nodes));
 
 		if (new_nodes) {
 			unsigned int j;