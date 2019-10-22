haproxy/haproxy

BUG/MEDIUM: namespace: fix fd leak in master-worker mode
When namespaces are used in the configuration, the respective namespace handles
are opened during config parsing and stored in an ebtree for lookup later.

Unfortunately, when the master process re-execs itself these file descriptors
were not closed, effectively leaking the fds and preventing destruction of
namespaces no longer present in the configuration.

This change fixes this issue by opening the namespace file handles as
close-on-exec, making sure that they will be closed during re-exec.

src/namespace.c
@@ -24,7 +24,7 @@ static int open_named_namespace(const char *ns_name)
 {
 	if (chunk_printf(&trash, "/var/run/netns/%s", ns_name) < 0)
 		return -1;
-	return open(trash.area, O_RDONLY);
+	return open(trash.area, O_RDONLY | O_CLOEXEC);
 }
 
 static int default_namespace = -1;
@@ -33,7 +33,7 @@ static int init_default_namespace()
 {
 	if (chunk_printf(&trash, "/proc/%d/ns/net", getpid()) < 0)
 		return -1;
-	default_namespace = open(trash.area, O_RDONLY);
+	default_namespace = open(trash.area, O_RDONLY | O_CLOEXEC);
 	return default_namespace;
 }
 

jiangwenyuan/nuster

(cherry picked from commit 538aa7168fca1adf2ecd0aa4a47e6b8856275f55)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit 59af61a19493ccc50e3815d84c9323762cf28fcd)
(cherry picked from commit a7f2abde42a04faba327b552d38749fe001e22b5)
[wt: adjusted to old buffer API]
Signed-off-by: Willy Tarreau <w@1wt.eu>

src/namespace.c
@@ -27,7 +27,7 @@ static int open_named_namespace(const char *ns_name)
 {
 	if (chunk_printf(&trash, "/var/run/netns/%s", ns_name) < 0)
 		return -1;
-	return open(trash.str, O_RDONLY);
+	return open(trash.str, O_RDONLY | O_CLOEXEC);
 }
 
 static int default_namespace = -1;
@@ -36,7 +36,7 @@ static int init_default_namespace()
 {
 	if (chunk_printf(&trash, "/proc/%d/ns/net", getpid()) < 0)
 		return -1;
-	default_namespace = open(trash.str, O_RDONLY);
+	default_namespace = open(trash.str, O_RDONLY | O_CLOEXEC);
 	return default_namespace;
 }
 