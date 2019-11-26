a2043ca7f5d13e3e204b6e959716682c4ce9279e
Jiang Wenyuan <koubunen@gmail.com>
2019-11-26 23:11:49 +0900

    Add nst_cache_build_etag
    (cherry picked from commit f8f08259d41f76f5db2ce36d2706327bde574825)

--- include/nuster/cache.h
+++ include/nuster/cache.h
@@ -275,6 +275,9 @@ void nst_cache_persist_async();
 void nst_cache_build_last_modified(struct nst_cache_ctx *ctx, struct stream *s,
         struct http_msg *msg);
 
+void nst_cache_build_etag(struct nst_cache_ctx *ctx, struct stream *s,
+        struct http_msg *msg);
+
 
 /* manager */
 int nst_cache_purge(struct stream *s, struct channel *req, struct proxy *px);
