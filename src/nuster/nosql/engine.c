0723fdc4f17d49a590574ec514e635336f36cb8a
Jiang Wenyuan <koubunen@gmail.com>
2020-04-11 14:21:15 +0900

    Set entry.disk.file on finish

--- src/nuster/nosql/engine.c
+++ src/nuster/nosql/engine.c
@@ -747,6 +747,8 @@ nst_nosql_finish(hpx_stream_t *s, hpx_http_msg_t *msg, nst_ctx_t *ctx) {
                     ctx->entry->expire) == NST_OK) {
 
             ctx->entry->state = NST_DICT_ENTRY_STATE_VALID;
+
+            ctx->entry->store.disk.file = ctx->store.disk.file;
         }
     }
 
