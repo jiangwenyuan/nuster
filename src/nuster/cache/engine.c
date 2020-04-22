205c986800f267eef4745335b64e0e7c8a61f052
Jiang Wenyuan <koubunen@gmail.com>
2020-04-22 09:21:25 +0900

    Payload does not send completely
    (cherry picked from commit f14b35cfd9579060cc5eada1d3154f82f6b4848f)

--- src/nuster/cache/engine.c
+++ src/nuster/cache/engine.c
@@ -223,6 +223,8 @@ _nst_cache_disk_handler(hpx_appctx_t *appctx) {
 
             if(appctx->ctx.nuster.store.disk.payload_len == 0) {
                 appctx->st1 = NST_DISK_APPLET_EOP;
+            } else {
+                break;
             }
 
         case NST_DISK_APPLET_EOP:
