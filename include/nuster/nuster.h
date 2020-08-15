f2dce57bda56d3a1879be0ceb93bc8b71089dd55
Jiang Wenyuan <koubunen@gmail.com>
2020-08-15 10:56:23 +0900

    Released v5.2.2.22

--- include/nuster/nuster.h
+++ include/nuster/nuster.h
@@ -22,7 +22,7 @@
 #ifndef _NUSTER_H
 #define _NUSTER_H
 
-#define NUSTER_VERSION    "5.2.1.22"
+#define NUSTER_VERSION    "5.2.2.22"
 #define NUSTER_COPYRIGHT  "2017-present, Jiang Wenyuan, <koubunen AT gmail DOT com >"
 
 #include <haproxy/applet-t.h>
