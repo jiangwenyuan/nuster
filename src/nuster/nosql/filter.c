jiangwenyuan/nuster

commit 051ef368f696201195a4c77092449103dbf9458c

Refactor
1. Introduce store(memory|disk) object, an object represents a complete
cache data
2. Rename memory to shmem
3. Rename store/ring to store/memory to unify naming
4. Introduce disk.obj in ctx/entry to unify struct
5. Mitigate code line break

src/nuster/nosql/filter.c
@@ -260,33 +260,33 @@ _nst_nosql_filter_http_headers(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
         /* 0: header unsent, 1: sent */
         appctx->st1 = 0;
 
-        appctx->ctx.nuster.store.ring.data = ctx->store.ring.data;
-        appctx->ctx.nuster.store.ring.item = ctx->store.ring.data->item;
+        appctx->ctx.nuster.store.memory.obj  = ctx->store.memory.obj;
+        appctx->ctx.nuster.store.memory.item = ctx->store.memory.obj->item;
 
         req->analysers &= ~AN_REQ_FLT_HTTP_HDRS;
         req->analysers &= ~AN_REQ_FLT_XFER_DATA;
-
         req->analysers |= AN_REQ_FLT_END;
+
         req->analyse_exp = TICK_ETERNITY;
 
         res->flags |= CF_NEVER_WAIT;
     }
 
     if(ctx->state == NST_CTX_STATE_HIT_DISK) {
+        char  *meta = ctx->store.disk.obj.meta;
+
         appctx->st0 = NST_NOSQL_APPCTX_STATE_HIT_DISK;
         appctx->st1 = NST_DISK_APPLET_HEADER;
 
-        appctx->ctx.nuster.store.disk.fd         = ctx->store.disk.fd;
-        appctx->ctx.nuster.store.disk.offset     = nst_disk_pos_header(&ctx->store.disk);
-        appctx->ctx.nuster.store.disk.header_len =
-            nst_disk_meta_get_header_len(ctx->store.disk.meta);
-        appctx->ctx.nuster.store.disk.payload_len =
-            nst_disk_meta_get_payload_len(ctx->store.disk.meta);
+        appctx->ctx.nuster.store.disk.fd          = ctx->store.disk.obj.fd;
+        appctx->ctx.nuster.store.disk.offset      = nst_disk_pos_header(&ctx->store.disk.obj);
+        appctx->ctx.nuster.store.disk.header_len  = nst_disk_meta_get_header_len(meta);
+        appctx->ctx.nuster.store.disk.payload_len = nst_disk_meta_get_payload_len(meta);
 
         req->analysers &= ~AN_REQ_FLT_HTTP_HDRS;
         req->analysers &= ~AN_REQ_FLT_XFER_DATA;
-
         req->analysers |= AN_REQ_FLT_END;
+
         req->analyse_exp = TICK_ETERNITY;
 
         res->flags |= CF_NEVER_WAIT;
@@ -347,10 +347,11 @@ _nst_nosql_filter_http_payload(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
         return 0;
     }
 
-    if((ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE)
-            && !(msg->chn->flags & CF_ISRESP)) {
+    if(msg->chn->flags & CF_ISRESP) {
 
-        len = nst_nosql_update(msg, ctx, offset, len);
+        if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
+            len = nst_nosql_append(msg, ctx, offset, len);
+        }
     }
 
     return len;
@@ -362,19 +363,21 @@ _nst_nosql_filter_http_end(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_msg_t
     hpx_appctx_t            *appctx = si_appctx(si);
     nst_ctx_t               *ctx    = filter->ctx;
 
-    if((ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE)
-            && !(msg->chn->flags & CF_ISRESP)) {
+    if(msg->chn->flags & CF_ISRESP) {
 
-        nst_nosql_finish(s, msg, ctx);
+        if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
 
-        if(ctx->state == NST_CTX_STATE_DONE) {
-            nst_debug(s, "[nosql] Create OK");
-            appctx->st0 = NST_NOSQL_APPCTX_STATE_END;
-        } else {
-            nst_debug(s, "[nosql] Create Failed");
-            appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR;
+            nst_nosql_finish(s, msg, ctx);
+
+            if(ctx->state == NST_CTX_STATE_DONE) {
+                nst_debug(s, "[nosql] Create OK");
+                appctx->st0 = NST_NOSQL_APPCTX_STATE_END;
+            } else {
+                nst_debug(s, "[nosql] Create Failed");
+                appctx->st0 = NST_NOSQL_APPCTX_STATE_ERROR;
+            }
+
         }
-
     }
 
     return 1;

src/nuster/cache/filter.c
@@ -109,7 +109,7 @@ _nst_cache_filter_detach(hpx_stream_t *s, hpx_filter_t *filter) {
         nst_stats_update_cache(ctx->state, ctx->txn.res.payload_len + ctx->txn.res.header_len);
 
         if(ctx->state == NST_CTX_STATE_HIT_MEMORY) {
-            nst_ring_data_detach(&nuster.cache->store.ring, ctx->store.ring.data);
+            nst_memory_obj_detach(&nuster.cache->store.memory, ctx->store.memory.obj);
         }
 
         if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
@@ -134,18 +134,19 @@ _nst_cache_filter_detach(hpx_stream_t *s, hpx_filter_t *filter) {
 
 static int
 _nst_cache_filter_http_headers(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_msg_t *msg) {
-    hpx_channel_t           *req = msg->chn;
-    hpx_channel_t           *res = &s->res;
-    hpx_proxy_t             *px  = s->be;
-    hpx_stream_interface_t  *si  = &s->si[1];
-    nst_ctx_t               *ctx = filter->ctx;
+    hpx_channel_t           *req  = msg->chn;
+    hpx_channel_t           *res  = &s->res;
+    hpx_proxy_t             *px   = s->be;
+    hpx_stream_interface_t  *si   = &s->si[1];
+    hpx_http_meth_t          meth = s->txn->meth;
+    nst_ctx_t               *ctx  = filter->ctx;
     hpx_htx_t               *htx;
 
     if(!(msg->chn->flags & CF_ISRESP)) {
         /* request */
 
         /* check http method */
-        if(s->txn->meth == HTTP_METH_OTHER) {
+        if(meth == HTTP_METH_OTHER) {
             ctx->state = NST_CTX_STATE_BYPASS;
         }
 
@@ -185,9 +186,7 @@ _nst_cache_filter_http_headers(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
 
                 if(!ctx->key->data) {
                     /* build key */
-                    if(nst_key_build(s, msg, ctx->rule, &ctx->txn, ctx->key, s->txn->meth)
-                            != NST_OK) {
-
+                    if(nst_key_build(s, msg, ctx->rule, &ctx->txn, ctx->key, meth) != NST_OK) {
                         ctx->state = NST_CTX_STATE_BYPASS;
 
                         return 1;
@@ -216,6 +215,7 @@ _nst_cache_filter_http_headers(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
                 }
 
                 if(ctx->state == NST_CTX_STATE_WAIT) {
+
                     if(ctx->prop->wait >= 0) {
                         nst_key_reset_flag(ctx->key);
                         nst_debug_end("WAIT");
@@ -259,9 +259,9 @@ _nst_cache_filter_http_headers(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
         }
 
         if(ctx->state == NST_CTX_STATE_WAIT) {
-            if(ctx->prop->wait == 0 || (ctx->prop->wait > 0
-                        && nst_time_now_ms() - ctx->ctime < ctx->prop->wait * 1000)) {
+            int  t = nst_time_now_ms() - ctx->ctime;
 
+            if(ctx->prop->wait == 0 || (ctx->prop->wait > 0 && t < ctx->prop->wait * 1000)) {
                 usleep(1);
                 ctx->state = NST_CTX_STATE_INIT;
 
@@ -382,10 +382,11 @@ _nst_cache_filter_http_payload(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
         return 0;
     }
 
-    if((ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE)
-            && (msg->chn->flags & CF_ISRESP)) {
+    if(msg->chn->flags & CF_ISRESP) {
 
-        len = nst_cache_update(msg, ctx, offset, len);
+        if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
+            len = nst_cache_append(msg, ctx, offset, len);
+        }
     }
 
     return len;
@@ -396,13 +397,15 @@ _nst_cache_filter_http_end(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_msg_t
 
     nst_ctx_t  *ctx = filter->ctx;
 
-    if((ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE)
-            && (msg->chn->flags & CF_ISRESP)) {
+    if(msg->chn->flags & CF_ISRESP) {
 
-        if(nst_cache_finish(ctx) == NST_OK) {
-            nst_debug(s, "[cache] Create OK");
-        } else {
-            nst_debug(s, "[cache] Created Failed");
+        if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
+
+            if(nst_cache_finish(ctx) == NST_OK) {
+                nst_debug(s, "[cache] Create OK");
+            } else {
+                nst_debug(s, "[cache] Created Failed");
+            }
         }
     }
 

Makefile
@@ -833,8 +833,8 @@ NUSTER_OBJS = src/nuster/cache/engine.o src/nuster/cache/filter.o             \
               src/nuster/nosql/engine.o src/nuster/nosql/filter.o             \
               src/nuster/manager/stats.o src/nuster/manager/engine.o          \
               src/nuster/manager/purger.o                                     \
-              src/nuster/store/ring.o src/nuster/store/disk.o                 \
-              src/nuster/memory.o src/nuster/parser.o src/nuster/http.o       \
+              src/nuster/store/memory.o src/nuster/store/disk.o               \
+              src/nuster/shmem.o src/nuster/parser.o src/nuster/http.o        \
               src/nuster/key.o src/nuster/dict.o src/nuster/sample.o	      \
               src/nuster/misc.o src/nuster/nuster.o
 

include/nuster/cache.h
@@ -33,9 +33,7 @@ void nst_cache_init();
 void nst_cache_housekeeping();
 
 void nst_cache_create(hpx_http_msg_t *msg, nst_ctx_t *ctx);
-unsigned int nst_cache_update(hpx_http_msg_t *msg, nst_ctx_t *ctx, unsigned int offset,
-        unsigned int len);
-
+int nst_cache_append(hpx_http_msg_t *msg, nst_ctx_t *ctx, unsigned int offset, unsigned int len);
 int nst_cache_finish(nst_ctx_t *ctx);
 void nst_cache_abort(nst_ctx_t *ctx);
 int nst_cache_exists(nst_ctx_t *ctx);

include/nuster/common.h
@@ -27,6 +27,7 @@ typedef struct dirent                   nst_dirent_t;
 typedef struct sample_fetch_kw_list     hpx_sample_fetch_kw_list_t;
 typedef struct stream_interface         hpx_stream_interface_t;
 typedef struct http_hdr_ctx             hpx_http_hdr_ctx_t;
+typedef enum   http_meth_t              hpx_http_meth_t;
 typedef enum   htx_blk_type             hpx_htx_blk_type_t;
 typedef struct flt_conf                 hpx_flt_conf_t;
 typedef struct http_msg                 hpx_http_msg_t;
@@ -95,25 +96,25 @@ typedef struct nst_core                 nst_core_t;
 #define NST_DEFAULT_CODE               "200"
 
 enum {
-    NST_STATUS_UNDEFINED = -1,
-    NST_STATUS_OFF       =  0,
-    NST_STATUS_ON        =  1,
+    NST_STATUS_UNDEFINED        = -1,
+    NST_STATUS_OFF              =  0,
+    NST_STATUS_ON               =  1,
 };
 
 enum {
-    NST_MODE_CACHE       = 1,
-    NST_MODE_NOSQL       = 2,
+    NST_MODE_CACHE              = 1,
+    NST_MODE_NOSQL              = 2,
 };
 
 enum {
-    NST_RULE_DISABLED    = 0,
-    NST_RULE_ENABLED     = 1,
+    NST_RULE_DISABLED           = 0,
+    NST_RULE_ENABLED            = 1,
 };
 
 enum {
-    NST_TIME_OK          = 0,
-    NST_TIME_OVER        = 1,
-    NST_TIME_ERR         = 2,
+    NST_TIME_OK                 = 0,
+    NST_TIME_OVER               = 1,
+    NST_TIME_ERR                = 2,
 };
 
 enum {
@@ -126,7 +127,7 @@ enum {
 
 enum nst_key_element_type {
     /* method: GET, POST... */
-    NST_KEY_ELEMENT_METHOD = 1,
+    NST_KEY_ELEMENT_METHOD      = 1,
 
     /* scheme: http, https */
     NST_KEY_ELEMENT_SCHEME,
@@ -260,7 +261,7 @@ typedef struct nst_flt_conf {
 /* get current timestamp in milliseconds */
 static inline uint64_t
 nst_time_now_ms() {
-    struct timespec ts;
+    struct timespec  ts;
 
     clock_gettime(CLOCK_REALTIME, &ts);
 
@@ -270,7 +271,7 @@ nst_time_now_ms() {
 /* get current timestamp in nanoseconds */
 static inline uint64_t
 nst_time_now_ns() {
-    struct timespec ts;
+    struct timespec  ts;
 
     clock_gettime(CLOCK_REALTIME, &ts);
 

include/nuster/core.h
@@ -66,10 +66,12 @@ typedef struct nst_ctx {
 
     struct {
         struct {
-            nst_ring_data_t    *data;
-            nst_ring_item_t    *item;
-        } ring;
-        nst_disk_data_t         disk;
+            nst_memory_obj_t   *obj;
+            nst_memory_item_t  *item;
+        } memory;
+        struct {
+            nst_disk_obj_t      obj;
+        } disk;
     } store;
 
     nst_rule_prop_t            *prop;
@@ -82,7 +84,7 @@ typedef struct nst_ctx {
 } nst_ctx_t;
 
 struct nst_core {
-    nst_memory_t               *memory;
+    nst_shmem_t                *shmem;
     hpx_ist_t                   root;
 
     nst_dict_t                  dict;

include/nuster/dict.h
@@ -70,9 +70,9 @@ typedef struct nst_dict_entry {
 
     struct {
         struct {
-            nst_ring_data_t    *data;
-            nst_ring_item_t    *item;
-        } ring;
+            nst_memory_obj_t   *obj;
+            nst_memory_item_t  *item;
+        } memory;
         struct {
             char               *file;
         } disk;
@@ -80,7 +80,7 @@ typedef struct nst_dict_entry {
 } nst_dict_entry_t;
 
 typedef struct nst_dict {
-    nst_memory_t               *memory;
+    nst_shmem_t                *shmem;
 
     nst_dict_entry_t          **entry;
     uint64_t                    size;           /* number of entries */
@@ -162,7 +162,7 @@ nst_dict_entry_valid(nst_dict_entry_t *entry) {
     return 0;
 }
 
-int nst_dict_init(nst_dict_t *dict, nst_store_t *store, nst_memory_t *memory, uint64_t dict_size);
+int nst_dict_init(nst_dict_t *dict, nst_store_t *store, nst_shmem_t *shmem, uint64_t dict_size);
 void nst_dict_cleanup(nst_dict_t *dict);
 
 nst_dict_entry_t *nst_dict_get(nst_dict_t *dict, nst_key_t *key);

include/nuster/disk.h
@@ -90,16 +90,16 @@ enum {
     NST_DISK_APPLET_END,
 };
 
-typedef struct nst_disk_data {
+typedef struct nst_disk_object {
     char               *file;               /* disk file */
     int                 fd;
     uint64_t            offset;
     char                meta[NST_DISK_META_SIZE];
-} nst_disk_data_t;
+} nst_disk_obj_t;
 
 
 typedef struct nst_disk {
-    nst_memory_t       *memory;
+    nst_shmem_t        *shmem;
     hpx_ist_t           root;               /* disk root directory */
     int                 loaded;
     int                 idx;
@@ -125,19 +125,17 @@ nst_disk_path_file_len(hpx_ist_t root) {
 }
 
 static inline int
-nst_disk_remove(const char *file) {
+nst_disk_file_remove(const char *file) {
     return remove(file);
 }
 
-int nst_disk_mkdir(char *path);
-
 static inline int
-nst_disk_data_create(const char *pathname) {
+nst_disk_file_create(const char *pathname) {
     return open(pathname, O_CREAT | O_WRONLY, 0600);
 }
 
 static inline int
-nst_disk_open(const char *pathname) {
+nst_disk_file_open(const char *pathname) {
     return open(pathname, O_RDONLY);
 }
 
@@ -345,172 +343,167 @@ nst_disk_meta_check_stale(char *p) {
 }
 
 static inline int
-nst_disk_pos_proxy(nst_disk_data_t *data) {
-    return NST_DISK_POS_KEY + nst_disk_meta_get_key_len(data->meta);
+nst_disk_pos_proxy(nst_disk_obj_t *obj) {
+    return NST_DISK_POS_KEY + nst_disk_meta_get_key_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_rule(nst_disk_data_t *data) {
+nst_disk_pos_rule(nst_disk_obj_t *obj) {
     return NST_DISK_POS_KEY
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_host(nst_disk_data_t *data) {
+nst_disk_pos_host(nst_disk_obj_t *obj) {
     return NST_DISK_POS_KEY
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta)
-        + nst_disk_meta_get_rule_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta)
+        + nst_disk_meta_get_rule_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_path(nst_disk_data_t *data) {
+nst_disk_pos_path(nst_disk_obj_t *obj) {
     return NST_DISK_POS_KEY
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta)
-        + nst_disk_meta_get_rule_len(data->meta)
-        + nst_disk_meta_get_host_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta)
+        + nst_disk_meta_get_rule_len(obj->meta)
+        + nst_disk_meta_get_host_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_etag(nst_disk_data_t *data) {
+nst_disk_pos_etag(nst_disk_obj_t *obj) {
     return NST_DISK_POS_KEY
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta)
-        + nst_disk_meta_get_rule_len(data->meta)
-        + nst_disk_meta_get_host_len(data->meta)
-        + nst_disk_meta_get_path_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta)
+        + nst_disk_meta_get_rule_len(obj->meta)
+        + nst_disk_meta_get_host_len(obj->meta)
+        + nst_disk_meta_get_path_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_last_modified(nst_disk_data_t *data) {
+nst_disk_pos_last_modified(nst_disk_obj_t *obj) {
     return NST_DISK_POS_KEY
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta)
-        + nst_disk_meta_get_rule_len(data->meta)
-        + nst_disk_meta_get_host_len(data->meta)
-        + nst_disk_meta_get_path_len(data->meta)
-        + nst_disk_meta_get_etag_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta)
+        + nst_disk_meta_get_rule_len(obj->meta)
+        + nst_disk_meta_get_host_len(obj->meta)
+        + nst_disk_meta_get_path_len(obj->meta)
+        + nst_disk_meta_get_etag_len(obj->meta);
 }
 
 static inline int
-nst_disk_pos_header(nst_disk_data_t *data) {
+nst_disk_pos_header(nst_disk_obj_t *obj) {
     return NST_DISK_META_SIZE
-        + nst_disk_meta_get_key_len(data->meta)
-        + nst_disk_meta_get_proxy_len(data->meta)
-        + nst_disk_meta_get_rule_len(data->meta)
-        + nst_disk_meta_get_host_len(data->meta)
-        + nst_disk_meta_get_path_len(data->meta)
-        + nst_disk_meta_get_etag_len(data->meta)
-        + nst_disk_meta_get_last_modified_len(data->meta);
+        + nst_disk_meta_get_key_len(obj->meta)
+        + nst_disk_meta_get_proxy_len(obj->meta)
+        + nst_disk_meta_get_rule_len(obj->meta)
+        + nst_disk_meta_get_host_len(obj->meta)
+        + nst_disk_meta_get_path_len(obj->meta)
+        + nst_disk_meta_get_etag_len(obj->meta)
+        + nst_disk_meta_get_last_modified_len(obj->meta);
 }
 
 static inline int
-nst_disk_write(nst_disk_data_t *data, char *buf, int len) {
-    ssize_t ret = pwrite(data->fd, buf, len, data->offset);
+nst_disk_write(nst_disk_obj_t *obj, char *buf, int len) {
+    ssize_t ret = pwrite(obj->fd, buf, len, obj->offset);
 
     if(ret != len) {
         return NST_ERR;
     }
 
-    data->offset += len;
+    obj->offset += len;
 
     return NST_OK;
 }
 
 static inline int
-nst_disk_write_meta(nst_disk_data_t *data) {
-    data->offset = 0;
+nst_disk_write_meta(nst_disk_obj_t *obj) {
+    obj->offset = 0;
 
-    return nst_disk_write(data, data->meta, NST_DISK_META_SIZE);
+    return nst_disk_write(obj, obj->meta, NST_DISK_META_SIZE);
 }
 
 static inline int
-nst_disk_write_key(nst_disk_data_t *data, nst_key_t *key) {
-    data->offset = NST_DISK_POS_KEY;
+nst_disk_write_key(nst_disk_obj_t *obj, nst_key_t *key) {
+    obj->offset = NST_DISK_POS_KEY;
 
-    return nst_disk_write(data, key->data, key->size);
+    return nst_disk_write(obj, key->data, key->size);
 }
 
 static inline int
-nst_disk_write_proxy(nst_disk_data_t *data, hpx_ist_t proxy) {
-    data->offset = nst_disk_pos_proxy(data);
+nst_disk_write_proxy(nst_disk_obj_t *obj, hpx_ist_t proxy) {
+    obj->offset = nst_disk_pos_proxy(obj);
 
-    return nst_disk_write(data, proxy.ptr, proxy.len);
+    return nst_disk_write(obj, proxy.ptr, proxy.len);
 }
 
 static inline int
-nst_disk_write_rule(nst_disk_data_t *data, hpx_ist_t rule) {
-    data->offset = nst_disk_pos_rule(data);
+nst_disk_write_rule(nst_disk_obj_t *obj, hpx_ist_t rule) {
+    obj->offset = nst_disk_pos_rule(obj);
 
-    return nst_disk_write(data, rule.ptr, rule.len);
+    return nst_disk_write(obj, rule.ptr, rule.len);
 }
 
 static inline int
-nst_disk_write_host(nst_disk_data_t *data, hpx_ist_t host) {
-    data->offset = nst_disk_pos_host(data);
+nst_disk_write_host(nst_disk_obj_t *obj, hpx_ist_t host) {
+    obj->offset = nst_disk_pos_host(obj);
 
-    return nst_disk_write(data, host.ptr, host.len);
+    return nst_disk_write(obj, host.ptr, host.len);
 }
 
 static inline int
-nst_disk_write_path(nst_disk_data_t *data, hpx_ist_t path) {
-    data->offset = nst_disk_pos_path(data);
+nst_disk_write_path(nst_disk_obj_t *obj, hpx_ist_t path) {
+    obj->offset = nst_disk_pos_path(obj);
 
-    return nst_disk_write(data, path.ptr, path.len);
+    return nst_disk_write(obj, path.ptr, path.len);
 }
 
 static inline int
-nst_disk_write_etag(nst_disk_data_t *data, hpx_ist_t etag) {
-    data->offset = nst_disk_pos_etag(data);
+nst_disk_write_etag(nst_disk_obj_t *obj, hpx_ist_t etag) {
+    obj->offset = nst_disk_pos_etag(obj);
 
-    return nst_disk_write(data, etag.ptr, etag.len);
+    return nst_disk_write(obj, etag.ptr, etag.len);
 }
 
 static inline int
-nst_disk_write_last_modified(nst_disk_data_t *data, hpx_ist_t lm) {
-    data->offset = nst_disk_pos_last_modified(data);
+nst_disk_write_last_modified(nst_disk_obj_t *obj, hpx_ist_t lm) {
+    obj->offset = nst_disk_pos_last_modified(obj);
 
-    return nst_disk_write(data, lm.ptr, lm.len);
+    return nst_disk_write(obj, lm.ptr, lm.len);
 }
 
-int nst_disk_read_key(nst_disk_t *disk, nst_disk_data_t *data, nst_key_t *key);
-int nst_disk_read_proxy(nst_disk_data_t *data, hpx_ist_t proxy);
-int nst_disk_read_rule(nst_disk_data_t *data, hpx_ist_t rule);
-int nst_disk_read_host(nst_disk_data_t *data, hpx_ist_t host);
-int nst_disk_read_path(nst_disk_data_t *data, hpx_ist_t path);
-int nst_disk_read_etag(nst_disk_data_t *data, hpx_ist_t etag);
-int nst_disk_read_last_modified(nst_disk_data_t *data, hpx_ist_t last_modified);
+int nst_disk_read_key(nst_disk_t *disk, nst_disk_obj_t *obj, nst_key_t *key);
+int nst_disk_read_proxy(nst_disk_obj_t *obj, hpx_ist_t proxy);
+int nst_disk_read_rule(nst_disk_obj_t *obj, hpx_ist_t rule);
+int nst_disk_read_host(nst_disk_obj_t *obj, hpx_ist_t host);
+int nst_disk_read_path(nst_disk_obj_t *obj, hpx_ist_t path);
+int nst_disk_read_etag(nst_disk_obj_t *obj, hpx_ist_t etag);
+int nst_disk_read_last_modified(nst_disk_obj_t *obj, hpx_ist_t last_modified);
 
-int nst_disk_init(hpx_ist_t root, nst_disk_t *disk, nst_memory_t *memory, int clean_temp);
+int nst_disk_init(nst_disk_t *disk, hpx_ist_t root, nst_shmem_t *shmem, int clean_temp);
 void nst_disk_load(nst_core_t *core);
 void nst_disk_cleanup(nst_core_t *core);
-
-DIR *nst_disk_opendir_by_idx(hpx_ist_t root, char *path, int idx);
-nst_dirent_t *nst_disk_dir_next(DIR *dir);
-int nst_disk_data_valid(nst_disk_data_t *disk, nst_key_t *key);
-int nst_disk_data_exists(nst_disk_t *disk, nst_disk_data_t *data, nst_key_t *key);
-int nst_disk_purge_by_key(hpx_ist_t root, nst_disk_data_t *disk, nst_key_t *key);
+int nst_disk_purge_by_key(nst_disk_obj_t *disk, nst_key_t *key, hpx_ist_t root);
 int nst_disk_purge_by_path(char *path);
 void nst_disk_update_expire(char *file, uint64_t expire);
 
-int nst_disk_store_init(nst_disk_t *disk, nst_disk_data_t *data, nst_key_t *key,
+int nst_disk_obj_create(nst_disk_t *disk, nst_disk_obj_t *obj, nst_key_t *key,
         nst_http_txn_t *txn, nst_rule_prop_t *prop);
 
 static inline int
-nst_disk_store_add(nst_disk_t *disk, nst_disk_data_t *data, char *buf, int len) {
+nst_disk_obj_append(nst_disk_t *disk, nst_disk_obj_t *obj, char *buf, int len) {
 
-    if(nst_disk_write(data, buf, len) != NST_OK) {
-        if(data->fd != -1) {
-            close(data->fd);
-            data->fd = -1;
+    if(nst_disk_write(obj, buf, len) != NST_OK) {
+        if(obj->fd != -1) {
+            close(obj->fd);
+            obj->fd = -1;
         }
 
-        if(data->file) {
-            remove(data->file);
-            nst_memory_free(disk->memory, data->file);
-            data->file = NULL;
+        if(obj->file) {
+            remove(obj->file);
+            nst_shmem_free(disk->shmem, obj->file);
+            obj->file = NULL;
         }
 
         return NST_ERR;
@@ -520,21 +513,24 @@ nst_disk_store_add(nst_disk_t *disk, nst_disk_data_t *data, char *buf, int len) 
 }
 
 int
-nst_disk_store_end(nst_disk_t *disk, nst_disk_data_t *data, nst_key_t *key, nst_http_txn_t *txn,
+nst_disk_obj_finish(nst_disk_t *disk, nst_disk_obj_t *obj, nst_key_t *key, nst_http_txn_t *txn,
         uint64_t expire);
 
 static inline void
-nst_disk_store_abort(nst_disk_t *disk, nst_disk_data_t *data) {
-    if(data->fd != -1) {
-        close(data->fd);
-        data->fd = -1;
+nst_disk_obj_abort(nst_disk_t *disk, nst_disk_obj_t *obj) {
+    if(obj->fd != -1) {
+        close(obj->fd);
+        obj->fd = -1;
     }
 
-    if(data->file) {
-        remove(data->file);
-        nst_memory_free(disk->memory, data->file);
-        data->file = NULL;
+    if(obj->file) {
+        remove(obj->file);
+        nst_shmem_free(disk->shmem, obj->file);
+        obj->file = NULL;
     }
 }
+
+int nst_disk_obj_valid(nst_disk_obj_t *disk, nst_key_t *key);
+int nst_disk_obj_exists(nst_disk_t *disk, nst_disk_obj_t *obj, nst_key_t *key);
 
 #endif /* _NUSTER_DISK_H */

commit 81a9b7230279199ddb72b015517f8b45ba64a82a

Fix broken nosql function
Use request stage, introduced by 051ef368f696201195a4c77092449103dbf9458c

src/nuster/nosql/filter.c
@@ -341,7 +341,7 @@ _nst_nosql_filter_http_payload(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_m
         return 0;
     }
 
-    if(msg->chn->flags & CF_ISRESP) {
+    if(!(msg->chn->flags & CF_ISRESP)) {
 
         if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
             len = nst_nosql_append(msg, ctx, offset, len);
@@ -357,7 +357,7 @@ _nst_nosql_filter_http_end(hpx_stream_t *s, hpx_filter_t *filter, hpx_http_msg_t
     hpx_appctx_t            *appctx = si_appctx(si);
     nst_ctx_t               *ctx    = filter->ctx;
 
-    if(msg->chn->flags & CF_ISRESP) {
+    if(!(msg->chn->flags & CF_ISRESP)) {
 
         if(ctx->state == NST_CTX_STATE_CREATE || ctx->state == NST_CTX_STATE_UPDATE) {
 