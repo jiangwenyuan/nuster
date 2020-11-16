jiangwenyuan/nuster

commit bccd6d5f2336979977f3f79837c7133a40621504

Refactor http

src/nuster/http.c
@@ -10,9 +10,70 @@
  *
  */
 
+#include <common/http.h>
+
+#include <proto/http_htx.h>
+
 #include <nuster/http.h>
 
-int nst_req_find_param(char *query_beg, char *query_end, char *name, char **value, int *value_len) {
+struct nst_http_code nst_http_codes[NST_HTTP_SIZE] = {
+    [NST_HTTP_100] = {
+        .status = 100,
+        .code   = IST("100"),
+        .reason = IST("Continue"),
+        .length = IST(""),
+    },
+    [NST_HTTP_200] = {
+        .status = 200,
+        .code   = IST("200"),
+        .reason = IST("OK"),
+        .length = IST("2"),
+    },
+    [NST_HTTP_304] = {
+        .status = 304,
+        .code   = IST("304"),
+        .reason = IST("Not Modified"),
+        .length = IST(""),
+    },
+    [NST_HTTP_400] = {
+        .status = 400,
+        .code   = IST("400"),
+        .reason = IST("Bad Request"),
+        .length = IST("11"),
+    },
+    [NST_HTTP_404] = {
+        .status = 404,
+        .code   = IST("404"),
+        .reason = IST("Not Found"),
+        .length = IST("9"),
+    },
+    [NST_HTTP_405] = {
+        .status = 405,
+        .code   = IST("405"),
+        .reason = IST("Method Not Allowed"),
+        .length = IST("18"),
+    },
+    [NST_HTTP_412] = {
+        .status = 412,
+        .code   = IST("412"),
+        .reason = IST("Precondition Failed"),
+        .length = IST("19"),
+    },
+    [NST_HTTP_500] = {
+        .status = 500,
+        .code   = IST("500"),
+        .reason = IST("Internal Server Error"),
+        .length = IST("21"),
+    },
+    [NST_HTTP_507] = {
+        .status = 507,
+        .code   = IST("507"),
+        .reason = IST("Insufficient storage"),
+        .length = IST("20"),
+    },
+};
+
+int nst_http_find_param(char *query_beg, char *query_end, char *name, char **val, int *val_len) {
     char equal   = '=';
     char and     = '&';
     char *ptr    = query_beg;
@@ -23,11 +84,11 @@ int nst_req_find_param(char *query_beg, char *query_end, char *name, char **valu
         if(!memcmp(ptr, name, name_len) && *(ptr + name_len) == equal) {
 
             if(ptr == query_beg || *(ptr - 1) == and) {
-                ptr    = ptr + name_len + 1;
-                *value = ptr;
+                ptr  = ptr + name_len + 1;
+                *val = ptr;
 
                 while(ptr < query_end && *ptr != and) {
-                    (*value_len)++;
+                    (*val)++;
                     ptr++;
                 }
 
@@ -42,7 +103,7 @@ int nst_req_find_param(char *query_beg, char *query_end, char *name, char **valu
     return NST_ERR;
 }
 
-int nst_data_element_to_htx(struct nst_data_element *element, struct htx *htx) {
+int nst_http_data_element_to_htx(struct nst_data_element *element, struct htx *htx) {
     struct htx_blk *blk;
     char *ptr;
     uint32_t blksz, sz, info;
@@ -68,182 +129,238 @@ int nst_data_element_to_htx(struct nst_data_element *element, struct htx *htx) {
     return NST_OK;
 }
 
-void nst_res_304(struct stream *s, struct ist last_modified, struct ist etag) {
-
+void nst_http_reply(struct stream *s, int idx) {
+    struct stream_interface *si = &s->si[1];
     struct channel *res = &s->res;
-    struct htx *htx = htx_from_buf(&res->buf);
+    struct htx *htx;
     struct htx_sl *sl;
-    struct ist code;
-    int status;
-    unsigned int flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11);
-    size_t data;
+    unsigned int flags;
 
-    status = 304;
-    code = ist("304");
+    b_reset(&res->buf);
 
-    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"), code,
-            ist("Not Modified"));
+    htx = htx_from_buf(&res->buf);
 
-    if(!sl) {
-        goto fail;
-    }
+    flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11|HTX_SL_F_XFER_LEN);
+    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"),
+            nst_http_codes[idx].code, nst_http_codes[idx].reason);
 
-    sl->info.res.status = status;
-    s->txn->status = status;
+    sl->info.res.status = nst_http_codes[idx].status;
 
-    if(!htx_add_header(htx, ist("Last-Modified"), last_modified)
-            || !htx_add_header(htx, ist("ETag"), etag)) {
+    htx_add_header(htx, ist("Content-Length"), nst_http_codes[idx].length);
 
-        goto fail;
-    }
+    htx_add_endof(htx, HTX_BLK_EOH);
 
-    if(!htx_add_endof(htx, HTX_BLK_EOH)) {
-        goto fail;
-    }
+    htx_add_data_atonce(htx, nst_http_codes[idx].reason);
 
-    if(!htx_add_endof(htx, HTX_BLK_EOM)) {
-        goto fail;
-    }
-
-    data = htx->data - co_data(res);
-    c_adv(res, data);
-    res->total += data;
-
-    channel_auto_read(&s->req);
-    channel_abort(&s->req);
-    channel_auto_close(&s->req);
-    channel_htx_erase(&s->req, htxbuf(&s->req.buf));
-
-    res->wex = tick_add_ifset(now_ms, res->wto);
-    channel_auto_read(res);
-    channel_auto_close(res);
-    channel_shutr_now(res);
-    return;
-
-fail:
-    channel_htx_truncate(res, htx);
-}
-
-void nst_res_412(struct stream *s) {
-
-    struct channel *res = &s->res;
-    struct htx *htx = htx_from_buf(&res->buf);
-    struct htx_sl *sl;
-    struct ist code, body;
-    int status;
-    unsigned int flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11);
-    size_t data;
-
-    status = 412;
-    code = ist("412");
-    body = ist("412 Precondition Failed");
-
-    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"), code,
-            ist("Precondition Failed"));
-
-    if(!sl) {
-        goto fail;
-    }
-
-    sl->info.res.status = status;
-    s->txn->status = status;
-
-    if(!htx_add_header(htx, ist("Content-Length"), ist("23"))) {
-        goto fail;
-    }
-
-    if(!htx_add_endof(htx, HTX_BLK_EOH)) {
-        goto fail;
-    }
-
-    while(body.len) {
-        size_t sent = htx_add_data(htx, body);
-
-        if(!sent) {
-            goto fail;
-        }
-
-        body.ptr += sent;
-        body.len -= sent;
-    }
-
-    if(!htx_add_endof(htx, HTX_BLK_EOM)) {
-        goto fail;
-    }
-
-    data = htx->data - co_data(res);
-    c_adv(res, data);
-    res->total += data;
-
-    channel_auto_read(&s->req);
-    channel_abort(&s->req);
-    channel_auto_close(&s->req);
-    channel_htx_erase(&s->req, htxbuf(&s->req.buf));
-
-    res->wex = tick_add_ifset(now_ms, res->wto);
-    channel_auto_read(res);
-    channel_auto_close(res);
-    channel_shutr_now(res);
-    return;
-
-fail:
-    channel_htx_truncate(res, htx);
-}
-
-void nst_res_simple(struct stream *s, int status) {
-
-    struct channel *res = &s->res;
-    struct htx *htx = htx_from_buf(&res->buf);
-    struct htx_sl *sl;
-    struct ist code;
-    unsigned int flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11);
-
-    switch(status) {
-        case 200:
-            code = ist("200");
-            break;
-        case 500:
-            code = ist("500");
-            break;
-        case 405:
-            code = ist("405");
-            break;
-        case 404:
-            code = ist("404");
-            break;
-        case 400:
-            code = ist("400");
-            break;
-        case 507:
-            code = ist("507");
-            break;
-        default:
-            code = ist("500");
-    }
-
-    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"), code,
-            ist2(http_get_reason(status), strlen(http_get_reason(status))));
-
-    if(!sl) {
-        goto fail;
-    }
-
-    sl->info.res.status = status;
-    s->txn->status = status;
-
-    if(!htx_add_endof(htx, HTX_BLK_EOH)) {
-        goto fail;
-    }
-
-    if(!htx_add_endof(htx, HTX_BLK_EOM)) {
-        goto fail;
-    }
+    htx_add_endof(htx, HTX_BLK_EOM);
 
     channel_add_input(res, htx->data);
+
+    if(!(res->flags & CF_SHUTR)) {
+        res->flags |= CF_READ_NULL;
+        si_shutr(si);
+    }
+
     htx_to_buf(htx, &res->buf);
-    return;
+}
+
+int nst_http_reply_100(struct stream *s) {
+    struct channel *res = &s->res;
+    struct htx *htx     = htx_from_buf(&res->buf);
+    struct htx_sl *sl;
+    size_t data;
+    unsigned int flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11|HTX_SL_F_XFER_LEN|HTX_SL_F_BODYLESS);
+
+    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"),
+            nst_http_codes[NST_HTTP_100].code, nst_http_codes[NST_HTTP_100].reason);
+
+    if(!sl) {
+        goto fail;
+    }
+
+    sl->info.res.status = nst_http_codes[NST_HTTP_100].status;
+
+    if(!htx_add_endof(htx, HTX_BLK_EOH)) {
+        goto fail;
+    }
+
+    data = htx->data - co_data(res);
+    c_adv(res, data);
+    res->total += data;
+
+    return 0;
 
 fail:
     channel_htx_truncate(res, htx);
+
+    return -1;
 }
+
+void nst_http_reply_304(struct stream *s, struct ist last_modified, struct ist etag) {
+    struct stream_interface *si = &s->si[1];
+    struct channel *res = &s->res;
+    struct htx *htx;
+    struct htx_sl *sl;
+    unsigned int flags = (HTX_SL_F_IS_RESP|HTX_SL_F_VER_11|HTX_SL_F_XFER_LEN|HTX_SL_F_BODYLESS);
+
+    b_reset(&res->buf);
+
+    htx = htx_from_buf(&res->buf);
+
+    sl = htx_add_stline(htx, HTX_BLK_RES_SL, flags, ist("HTTP/1.1"),
+            nst_http_codes[NST_HTTP_304].code, nst_http_codes[NST_HTTP_304].reason);
+
+    sl->info.res.status = nst_http_codes[NST_HTTP_304].status;
+
+    htx_add_header(htx, ist("Last-Modified"), last_modified);
+
+    htx_add_header(htx, ist("ETag"), etag);
+
+    htx_add_endof(htx, HTX_BLK_EOH);
+
+    htx_add_endof(htx, HTX_BLK_EOM);
+
+    channel_add_input(res, htx->data);
+
+    if(!(res->flags & CF_SHUTR)) {
+        res->flags |= CF_READ_NULL;
+        si_shutr(si);
+    }
+
+    htx_to_buf(htx, &res->buf);
+
+}
+
+int nst_http_handle_expect(struct stream *s, struct htx *htx, struct http_msg *msg) {
+
+    if(msg->msg_state == HTTP_MSG_BODY
+            && (msg->flags & HTTP_MSGF_VER_11)
+            && (msg->flags & (HTTP_MSGF_CNT_LEN|HTTP_MSGF_TE_CHNK))) {
+
+        struct ist hdr = { .ptr = "Expect", .len = 6 };
+        struct http_hdr_ctx ctx;
+
+        ctx.blk = NULL;
+        /* Expect is allowed in 1.1, look for it */
+        if(http_find_header(htx, hdr, &ctx, 0)
+                && unlikely(isteqi(ctx.value, ist2("100-continue", 12)))) {
+
+            if(nst_http_reply_100(s) == -1) {
+                return -1;
+            }
+
+            http_remove_header(htx, &ctx);
+        }
+    }
+
+    return 0;
+}
+
+/*
+ * return:
+ * 1: http_headers should return 1 immediately
+ * 0: http_headers should proceed
+ */
+int nst_http_handle_conditional_req(struct stream *s, struct htx *htx,
+        int test_last_modified, struct ist last_modified, int test_etag, struct ist etag) {
+
+    struct http_hdr_ctx hdr = { .blk = NULL };
+
+    int if_none_match     = -1;
+    int if_match          = -1;
+    int if_modified_since = -1;;
+
+    if(test_etag != NST_STATUS_ON && test_last_modified != NST_STATUS_ON) {
+        return 0;
+    }
+
+    if(test_etag == NST_STATUS_ON) {
+
+        while(http_find_header(htx, ist("If-Match"), &hdr, 0)) {
+
+            if_match = 412;
+
+            if(1 == hdr.value.len && *(hdr.value.ptr) == '*') {
+                if_match = 200;
+
+                break;
+            }
+
+            if(isteq(etag, hdr.value)) {
+                if_match = 200;
+
+                break;
+            }
+        }
+
+        if(if_match == 412) {
+            goto code412;
+        }
+    }
+
+    if(test_last_modified == NST_STATUS_ON) {
+
+        if(http_find_header(htx, ist("If-Unmodified-Since"), &hdr, 1)) {
+
+            if(!isteq(last_modified, hdr.value)) {
+                goto code412;
+            }
+        }
+    }
+
+    if(test_etag == NST_STATUS_ON) {
+
+        while(http_find_header(htx, ist("If-None-Match"), &hdr, 0)) {
+
+            if_none_match = 200;
+
+            if(1 == hdr.value.len && *(hdr.value.ptr) == '*') {
+                if_none_match = 304;
+
+                break;
+            }
+
+            if(isteq(etag, hdr.value)) {
+                if_none_match = 304;
+
+                break;
+            }
+        }
+    }
+
+    if(test_last_modified == NST_STATUS_ON) {
+
+        if(http_find_header(htx, ist("If-Modified-Since"), &hdr, 1)) {
+
+            if(isteq(last_modified, hdr.value)) {
+                if_modified_since = 304;
+            } else {
+                if_modified_since = 200;
+            }
+        }
+    }
+
+    if(if_none_match == 304 && if_modified_since != 200) {
+        goto code304;
+    }
+
+    if(if_none_match != 200 && if_modified_since == 304) {
+        goto code304;
+    }
+
+    return 0;
+
+code304:
+
+    nst_http_reply_304(s, last_modified, etag);
+
+    return 1;
+
+code412:
+
+    nst_http_reply(s, NST_HTTP_412);
+
+    return 1;
+}
+
 

include/nuster/cache.h
@@ -235,11 +235,8 @@ void nst_cache_persist_load();
 void nst_cache_persist_async();
 void nst_cache_build_etag(struct nst_cache_ctx *ctx, struct stream *s, struct http_msg *msg);
 
-void nst_cache_build_last_modified(struct nst_cache_ctx *ctx, struct stream *s,
-        struct http_msg *msg);
-
-int nst_cache_handle_conditional_req(struct nst_cache_ctx *ctx, struct stream *s,
-        struct http_msg *msg);
+void
+nst_cache_build_last_modified(struct nst_cache_ctx *ctx, struct stream *s, struct http_msg *msg);
 
 int nst_cache_update(struct nst_cache_ctx *ctx, struct http_msg *msg,
         unsigned int offset, unsigned int len);

include/nuster/http.h
@@ -31,29 +31,35 @@
 
 #include <nuster/common.h>
 
-/*
-static inline void nst_res_header_date(struct buffer *header) {
-    const char mon[12][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
-        "Aug", "Sep", "Oct", "Nov", "Dec" };
+enum {
+    NST_HTTP_100 = 0,
+    NST_HTTP_200,
+    NST_HTTP_304,
+    NST_HTTP_400,
+    NST_HTTP_404,
+    NST_HTTP_405,
+    NST_HTTP_412,
+    NST_HTTP_500,
+    NST_HTTP_507,
+    NST_HTTP_SIZE
+};
 
-    const char day[7][4]  = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+struct nst_http_code {
+    int        status;
+    struct ist code;
+    struct ist reason;
+    struct ist length;
+};
 
-    struct tm *tm;
-    time_t now;
-    time(&now);
-    tm = gmtime(&now);
-    chunk_appendf(header, "%.*s: %s, %02d %s %04d %02d:%02d:%02d GMT\r\n",
-            nst_headers.date.len, nst_headers.date.data, day[tm->tm_wday],
-            tm->tm_mday, mon[tm->tm_mon], 1900 + tm->tm_year,
-            tm->tm_hour, tm->tm_min, tm->tm_sec);
-}
-*/
+int nst_http_find_param(char *query_beg, char *query_end, char *name, char **val, int *val_len);
+int nst_http_data_element_to_htx(struct nst_data_element *element, struct htx *htx);
 
-int nst_req_find_param(char *query_beg, char *query_end, char *name, char **value, int *value_len);
-int nst_data_element_to_htx(struct nst_data_element *element, struct htx *htx);
+void nst_http_reply(struct stream *s, int idx);
+int nst_http_reply_100(struct stream *s);
+void nst_http_reply_304(struct stream *s, struct ist last_modified, struct ist etag);
 
-void nst_res_simple(struct stream *s, int status);
-void nst_res_304(struct stream *s, struct ist last_modified, struct ist etag);
-void nst_res_412(struct stream *s);
+int nst_http_handle_expect(struct stream *s, struct htx *htx, struct http_msg *msg);
+int nst_http_handle_conditional_req(struct stream *s, struct htx *htx,
+        int test_last_modified, struct ist last_modified, int test_etag, struct ist etag);
 
 #endif /* _NUSTER_HTTP_H */

include/nuster/nosql.h
@@ -33,7 +33,7 @@
 enum {
     NST_NOSQL_APPCTX_STATE_INIT = 0,
     NST_NOSQL_APPCTX_STATE_WAIT,
-    NST_NOSQL_APPCTX_STATE_HIT,
+    NST_NOSQL_APPCTX_STATE_HIT_MEMORY,
     NST_NOSQL_APPCTX_STATE_CREATE,
     NST_NOSQL_APPCTX_STATE_DELETED,
     NST_NOSQL_APPCTX_STATE_END,

src/nuster/cache/dict.c
@@ -111,9 +111,9 @@ struct nst_cache_entry *nst_cache_dict_set(struct nst_cache_ctx *ctx) {
     struct nst_cache_dict  *dict  = NULL;
     struct nst_cache_data  *data  = NULL;
     struct nst_cache_entry *entry = NULL;
-    int idx;
     struct nst_key key = { .data = NULL };
-    struct buffer buf = { .area = NULL };
+    struct buffer buf  = { .area = NULL };
+    int idx;
 
     dict = _nst_cache_dict_rehashing() ? &nuster.cache->dict[1] : &nuster.cache->dict[0];
 

src/nuster/cache/engine.c
@@ -30,8 +30,8 @@ static void _nst_cache_memory_handler(struct appctx *appctx) {
     struct stream_interface *si = appctx->owner;
     struct channel *req = si_oc(si);
     struct channel *res = si_ic(si);
+
     struct htx *req_htx, *res_htx;
-    struct buffer *errmsg;
     struct nst_data_element *element = NULL;
     int total = 0;
 
@@ -41,17 +41,17 @@ static void _nst_cache_memory_handler(struct appctx *appctx) {
     if(unlikely(si->state == SI_ST_DIS || si->state == SI_ST_CLO)) {
         appctx->ctx.nuster.cache.data->clients--;
 
-        goto err;
+        goto out;
     }
 
     /* Check if the input buffer is avalaible. */
-    if (!b_size(&res->buf)) {
+    if(!b_size(&res->buf)) {
         si_rx_room_blk(si);
 
-        goto err;
+        goto out;
     }
 
-    if (res->flags & (CF_SHUTW|CF_SHUTR|CF_SHUTW_NOW)) {
+    if(res->flags & (CF_SHUTW|CF_SHUTR|CF_SHUTW_NOW)) {
         appctx->ctx.nuster.cache.element = NULL;
     }
 
@@ -59,7 +59,7 @@ static void _nst_cache_memory_handler(struct appctx *appctx) {
         element = appctx->ctx.nuster.cache.element;
 
         while(element) {
-            if(nst_data_element_to_htx(element, res_htx) != NST_OK) {
+            if(nst_http_data_element_to_htx(element, res_htx) != NST_OK) {
                 si_rx_room_blk(si);
 
                 goto out;
@@ -71,7 +71,7 @@ static void _nst_cache_memory_handler(struct appctx *appctx) {
 
     } else {
 
-        if (!htx_add_endof(res_htx, HTX_BLK_EOM)) {
+        if(!htx_add_endof(res_htx, HTX_BLK_EOM)) {
             si_rx_room_blk(si);
 
             goto out;
@@ -79,13 +79,13 @@ static void _nst_cache_memory_handler(struct appctx *appctx) {
 
         appctx->ctx.nuster.cache.data->clients--;
 
-        if (!(res->flags & CF_SHUTR) ) {
+        if(!(res->flags & CF_SHUTR) ) {
             res->flags |= CF_READ_NULL;
             si_shutr(si);
         }
 
         /* eat the whole request */
-        if (co_data(req)) {
+        if(co_data(req)) {
             req_htx = htx_from_buf(&req->buf);
             co_htx_skip(req, req_htx, co_data(req));
             htx_to_buf(req_htx, &req->buf);
@@ -97,18 +97,6 @@ out:
     total = res_htx->data - total;
     channel_add_input(res, total);
     htx_to_buf(res_htx, &res->buf);
-
-    return;
-
-err:
-    /* Sent and HTTP error 500 */
-    b_reset(&res->buf);
-    errmsg = &http_err_chunks[HTTP_ERR_500];
-    res->buf.data = b_data(errmsg);
-    memcpy(res->buf.area, b_head(errmsg), b_data(errmsg));
-    res_htx = htx_from_buf(&res->buf);
-
-    total = 0;
 }
 
 /*
@@ -132,14 +120,14 @@ static void _nst_cache_disk_handler(struct appctx *appctx) {
     total = res_htx->data;
 
     if(unlikely(si->state == SI_ST_DIS || si->state == SI_ST_CLO)) {
-        return;
+        goto out;
     }
 
     /* Check if the input buffer is avalaible. */
     if(res->buf.size == 0) {
         si_rx_room_blk(si);
 
-        return;
+        goto out;
     }
 
     /* check that the output is not closed */
@@ -236,7 +224,7 @@ static void _nst_cache_disk_handler(struct appctx *appctx) {
 
         case NST_PERSIST_APPLET_EOM:
 
-            if (!htx_add_endof(res_htx, HTX_BLK_EOM)) {
+            if(!htx_add_endof(res_htx, HTX_BLK_EOM)) {
                 si_rx_room_blk(si);
 
                 goto out;
@@ -245,12 +233,12 @@ static void _nst_cache_disk_handler(struct appctx *appctx) {
             appctx->st1 = NST_PERSIST_APPLET_DONE;
         case NST_PERSIST_APPLET_DONE:
 
-            if (!(res->flags & CF_SHUTR) ) {
+            if(!(res->flags & CF_SHUTR) ) {
                 res->flags |= CF_READ_NULL;
                 si_shutr(si);
             }
 
-            if (co_data(req)) {
+            if(co_data(req)) {
                 req_htx = htx_from_buf(&req->buf);
                 co_htx_skip(req, req_htx, co_data(req));
                 htx_to_buf(req_htx, &req->buf);
@@ -269,8 +257,6 @@ out:
     total = res_htx->data - total;
     channel_add_input(res, total);
     htx_to_buf(res_htx, &res->buf);
-
-    return;
 }
 
 /*
@@ -645,7 +631,7 @@ int nst_cache_build_key(struct nst_cache_ctx *ctx, struct stream *s, struct http
                     char *v = NULL;
                     int v_l = 0;
 
-                    if(nst_req_find_param(ctx->req.query.ptr,
+                    if(nst_http_find_param(ctx->req.query.ptr,
                                 ctx->req.query.ptr + ctx->req.query.len,
                                 ck->data, &v, &v_l) == NST_OK) {
 
@@ -796,7 +782,7 @@ int nst_cache_build_purge_key(struct stream *s, struct http_msg *msg, struct nst
     sl = http_get_stline(htx);
     uri = htx_sl_req_uri(sl);
 
-    if (!uri.len || *uri.ptr != '/') {
+    if(!uri.len || *uri.ptr != '/') {
         return NST_ERR;
     }
 
@@ -1598,116 +1584,4 @@ nst_cache_build_last_modified(struct nst_cache_ctx *ctx, struct stream *s, struc
         }
     }
 }
-
-/*
- * return:
- * 1: http_headers should return 1 immediately
- * 0: http_headers should proceed
- */
-int nst_cache_handle_conditional_req(struct nst_cache_ctx *ctx, struct stream *s,
-        struct http_msg *msg) {
-
-    struct htx *htx;
-    struct http_hdr_ctx hdr = { .blk = NULL };
-    struct nst_rule *rule = ctx->rule;
-
-    int if_none_match     = -1;
-    int if_match          = -1;
-    int if_modified_since = -1;;
-
-    htx = htxbuf(&s->req.buf);
-
-    if(rule->etag != NST_STATUS_ON && rule->last_modified != NST_STATUS_ON) {
-        return 0;
-    }
-
-    if(rule->etag == NST_STATUS_ON) {
-
-        while(http_find_header(htx, ist("If-Match"), &hdr, 0)) {
-
-            if_match = 412;
-
-            if(1 == hdr.value.len && *(hdr.value.ptr) == '*') {
-                if_match = 200;
-
-                break;
-            }
-
-            if(isteq(ctx->res.etag, hdr.value)) {
-                if_match = 200;
-
-                break;
-            }
-        }
-
-        if(if_match == 412) {
-            goto code412;
-        }
-    }
-
-    if(rule->last_modified == NST_STATUS_ON) {
-
-        if(http_find_header(htx, ist("If-Unmodified-Since"), &hdr, 1)) {
-
-            if(!isteq(ctx->res.last_modified, hdr.value)) {
-                goto code412;
-            }
-        }
-    }
-
-    if(rule->etag == NST_STATUS_ON) {
-
-        while(http_find_header(htx, ist("If-None-Match"), &hdr, 0)) {
-
-            if_none_match = 200;
-
-            if(1 == hdr.value.len && *(hdr.value.ptr) == '*') {
-                if_none_match = 304;
-
-                break;
-            }
-
-            if(isteq(ctx->res.etag, hdr.value)) {
-                if_none_match = 304;
-
-                break;
-            }
-        }
-    }
-
-    if(rule->last_modified == NST_STATUS_ON) {
-
-        if(http_find_header(htx, ist("If-Modified-Since"), &hdr, 1)) {
-
-            if(isteq(ctx->res.last_modified, hdr.value)) {
-                if_modified_since = 304;
-            } else {
-                if_modified_since = 200;
-            }
-        }
-    }
-
-    if(if_none_match == 304 && if_modified_since != 200) {
-        goto code304;
-    }
-
-    if(if_none_match != 200 && if_modified_since == 304) {
-        goto code304;
-    }
-
-    return 0;
-
-code304:
-
-    nst_res_304(s, ctx->res.last_modified, ctx->res.etag);
-
-    return 1;
-
-code412:
-
-    nst_res_412(s);
-
-    return 1;
-}
-
 

src/nuster/cache/filter.c
@@ -136,6 +136,7 @@ _nst_cache_filter_http_headers(struct stream *s, struct filter *filter, struct h
     struct proxy *px            = s->be;
     struct stream_interface *si = &s->si[1];
     struct nst_cache_ctx *ctx   = filter->ctx;
+    struct htx *htx;
 
     if(!(msg->chn->flags & CF_ISRESP)) {
         /* request */
@@ -201,7 +202,11 @@ _nst_cache_filter_http_headers(struct stream *s, struct filter *filter, struct h
 
                     nst_debug2("HIT memory\n");
 
-                    if(nst_cache_handle_conditional_req(ctx, s, msg)) {
+                    htx = htxbuf(&req->buf);
+
+                    if(nst_http_handle_conditional_req(s, htx, ctx->rule->last_modified,
+                                ctx->res.last_modified, ctx->rule->etag, ctx->res.etag)) {
+
                         return 1;
                     }
 
@@ -236,7 +241,11 @@ _nst_cache_filter_http_headers(struct stream *s, struct filter *filter, struct h
                         }
                     }
 
-                    if(nst_cache_handle_conditional_req(ctx, s, msg)) {
+                    htx = htxbuf(&req->buf);
+
+                    if(nst_http_handle_conditional_req(s, htx, ctx->rule->last_modified,
+                                ctx->res.last_modified, ctx->rule->etag, ctx->res.etag)) {
+
                         return 1;
                     }
 
@@ -265,6 +274,7 @@ _nst_cache_filter_http_headers(struct stream *s, struct filter *filter, struct h
 
         if(ctx->state == NST_CACHE_CTX_STATE_HIT_MEMORY
                 || ctx->state == NST_CACHE_CTX_STATE_HIT_DISK) {
+
             nst_cache_hit(s, si, req, res, ctx);
         }
 
@@ -310,6 +320,7 @@ _nst_cache_filter_http_headers(struct stream *s, struct filter *filter, struct h
 
                 if(cc->code == s->txn->status) {
                     valid = 1;
+
                     break;
                 }
 
@@ -422,9 +433,7 @@ nst_smp_fetch_cache_hit(const struct arg *args, struct sample *smp, const char *
 
 static struct sample_fetch_kw_list nst_sample_fetch_keywords = {
     ILH, {
-        { "nuster.cache.hit", nst_smp_fetch_cache_hit, 0, NULL, SMP_T_BOOL,
-            SMP_USE_HRSHP
-        },
+        { "nuster.cache.hit", nst_smp_fetch_cache_hit, 0, NULL, SMP_T_BOOL, SMP_USE_HRSHP },
     }
 };
 

src/nuster/manager/engine.c
@@ -28,9 +28,10 @@ static int _nst_manager_set_state_ttl(struct stream *s, struct channel *req, str
 
     struct htx *htx = htxbuf(&s->req.buf);
     struct http_hdr_ctx hdr = { .blk = NULL };
+    int ret = NST_HTTP_400;
 
     if(state == -1 && ttl == -1) {
-        return 400;
+        goto end;
     }
 
     if(http_find_header(htx, ist("name"), &hdr, 0)) {
@@ -83,13 +84,17 @@ static int _nst_manager_set_state_ttl(struct stream *s, struct channel *req, str
         }
 
         if(found) {
-            return 200;
+            ret = NST_HTTP_200;
         } else {
-            return 404;
+            ret = NST_HTTP_404;
         }
     }
 
-    return 400;
+end:
+
+    nst_http_reply(s, ret);
+
+    return 1;
 }
 
 static int _nst_manager_check_uri(struct http_msg *msg) {
@@ -167,37 +172,16 @@ int nst_manager(struct stream *s, struct channel *req, struct proxy *px) {
                     nst_parse_time(hdr.value.ptr, hdr.value.len, (unsigned *)&ttl);
                 }
 
-                txn->status = _nst_manager_set_state_ttl(s, req, px, state, ttl);
+                return _nst_manager_set_state_ttl(s, req, px, state, ttl);
             } else if(txn->meth == HTTP_METH_DELETE) {
                 /* purge */
-                txn->status = nst_purger_advanced(s, req, px);
-
-                if(txn->status == 0) {
-                    return 0;
-                }
+                return nst_purger_advanced(s, req, px);
             } else {
                 return 0;
             }
         } else {
             return 0;
         }
-    }
-
-    switch(txn->status) {
-        case 200:
-            http_reply_and_close(s, txn->status, http_error_message(s));
-            break;
-        case 400:
-            http_reply_and_close(s, txn->status, http_error_message(s));
-            break;
-        case 404:
-            http_reply_and_close(s, txn->status, http_error_message(s));
-            break;
-        case 500:
-            http_reply_and_close(s, txn->status, http_error_message(s));
-            break;
-        default:
-            http_reply_and_close(s, txn->status, http_error_message(s));
     }
 
     return 1;

commit 16598e5afcc9836310939ab36e77f54452dade3d

Fix broken param_ parser which was introduced by bccd6d5f233

src/nuster/http.c
@@ -91,7 +91,7 @@ nst_http_find_param(char *query_beg, char *query_end, char *name, char **val, in
                 *val = ptr;
 
                 while(ptr < query_end && *ptr != and) {
-                    (*val)++;
+                    (*val_len)++;
                     ptr++;
                 }
 